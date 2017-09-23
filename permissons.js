1c067c8fcf0c60e525e80f65608d1106cf698fc6
minzoe <min16009@byui.edu>
2017-09-23 14:17:21 -0600

    Add files via upload

--- permissons.js
+++ permissons.js
@@ -1,68 +1,475 @@
-//Get JSON
-function getJSON(url) {
-    return fetch(url)
-        .then(function (response) {
-            return response.json();
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-}
-
-// Get the data from the json
-function getData() {
-    var url = "employee.json";
-    getJSON(url).then(function (data) {
-        console.log(data);
-    });
-}
-
-
 function logIn() {
-    getJSON("employee.json").then(function (data) {
-        //Select User
-        var email = document.getElementById('email').value;
-        console.log(email);
-        var user = "";
-        var id;
-        console.log(data);
-        var i = 0;
-        for (i = 0; i <= 21; i++) {
-            var e = data[i].email;
-            if (e == email) {
-                user = data[i].name;
-                id = i;
-            }
+    //Select User
+    var email = document.getElementById('input').value;
+    var user = "";
+    var id;
+    for (var i = 0; i <= 26; i++) {
+        var e = data[i].email;
+        if (e == email) {
+            user = data[i].name;
+            id = i;
         }
-        console.log(user);
-        console.log(id);
-        console.log(data[id].brightspace); 
-        display(id, user);
-    })
+    }
+    setLink(id);
+    //clear input
+    document.getElementById('input').value = "";
+    document.getElementById('input').setAttribute('placeholder', 'Enter Code')
+    var funcCode = 'enterCode(' + id + ')';
+    document.getElementById('button').setAttribute('onclick', funcCode);
+    document.getElementById('button').innerHTML = 'Enter Code';
+    console.log(user);
 }
 
-function display(id, user) {
-    getJSON('employee.json').then(function (data) {
-        //hide form
-        document.getElementById('form').classList.add('hide');
-        //display icons
-        if (data[id].brightspace == true) {
-            document.getElementById('bs').className = document.getElementById('bs').className.replace('hide', '');
-        }
-        if (data[id].workDay == true) {
-            document.getElementById('wd').className = document.getElementById('bs').className.replace('hide', 'show');
-        }
-    })
+function enterCode(id) {
+    var code = document.getElementById('input').value;
+    //Code Input
+    if (code == 'WorkDay') {
+        data[id].workDay = true;
+        data[id] = 5;
+        console.log(data[id]);
+        console.log(data[id].workDay);
+    }
+    document.getElementById('input').value = "";
+    setLink(id);
 }
 
+function setLink(id) {
+    //Create Hyperlinks
+    if (data[id].workDay == true) {
+        var icon = document.getElementById('wd');
+        wd.setAttribute('href', "https://www.myworkday.com/byuhi/d/home.htmld#selectedWorklet=501%24162");
+    } else {
+        var icon = document.getElementById('wd');
+        wd.setAttribute('href', "");
+    }
+    if (data[id].trello == true) {
+        var icon = document.getElementById('trel');
+        trel.setAttribute('href', "https://trello.com/");
+    } else {
+        var icon = document.getElementById('trel');
+        trel.setAttribute('href', "");
+    }
+    if (data[id].equella == true) {
+        var icon = document.getElementById('equ');
+        equ.setAttribute('href', "https://content.byui.edu/access/home.do");
+    } else {
+        var icon = document.getElementById('equ');
+        equ.setAttribute('href', "");
+    }
+    if (data[id].teamDynamix == true) {
+        var icon = document.getElementById('tdy');
+        tdy.setAttribute('href', "https://td.byui.edu");
+    } else {
+        var icon = document.getElementById('tdy');
+        tdy.setAttribute('href', "");
+    }
+    if (data[id].teamDrive == true) {
+        var icon = document.getElementById('tdr');
+        tdr.setAttribute('href', "https://drive.google.com/drive/folders/0AKiJtEpGJEXOUk9PVA");
+    } else {
+        var icon = document.getElementById('tdr');
+        tdr.setAttribute('href', "");
+    }
+    if (data[id].brightspace == true) {
+        var icon = document.getElementById('bs');
+        bs.setAttribute('href', "https://byui.brightspace.com/d2l/login?noredirect=true");
+    } else {
+        var icon = document.getElementById('bs');
+        bs.setAttribute('href', "");
+    }
+    if (data[id].microsoft == true) {
+        var icon = document.getElementById('micr');
+        micr.setAttribute('href', "https://www.office.com/1/?auth=2&amp;home=1&amp;from=PortalLanding&amp;client-request-id=77ffd374-bbac-4e2b-8a9d-9c1566dea2ed");
+    } else {
+        var icon = document.getElementById('micr');
+        micr.setAttribute('href', "");
+    }
+    if (data[id].staffHub == true) {
+        var icon = document.getElementById('sh');
+        sh.setAttribute('href', "https://staffhub.ms/app/TEAM_beb31523bee44c99b3a052306b0a71c4/schedules");
+    } else {
+        var icon = document.getElementById('sh');
+        sh.setAttribute('href', "");
+    }
+    if (data[id].canvas == true) {
+        var icon = document.getElementById('can');
+        can.setAttribute('href', "https://byui.instructure.com/login/canvas");
+    } else {
+        var icon = document.getElementById('can');
+        can.setAttribute('href', "");
+    }
+    if (data[id].employeeDirectory == true) {
+        var icon = document.getElementById('ed');
+        ed.setAttribute('href', "https://web.byui.edu/directory/employees/");
+    } else {
+        var icon = document.getElementById('ed');
+        ed.setAttribute('href', "");
+    }
+}
+
+
+//Get JSON
+var data = [{
+        "name": "Zoe Miner",
+        "email": "min16009@byui.edu",
+        "brightspace": true,
+        "trello": true,
+        "equella": true,
+        "teamDynamix": true,
+        "teamDrive": true,
+        "microsoft": true,
+        "workDay": false,
+        "staffHub": true,
+        "canvas": true,
+        "employeeDirectory": true
+        }, {
+        "name": "Jessica Heiner",
+        "email": "hei15001@byui.edu",
+        "brightspace": false,
+        "trello": false,
+        "equella": false,
+        "teamDynamix": false,
+        "teamDrive": false,
+        "microsoft": false,
+        "workDay": false,
+        "staffHub": false,
+        "canvas": false,
+        "employeeDirectory": false
+        }, {
+        "name": "Zac Mendenhall",
+        "email": "men12016@byui.edu",
+        "brightspace": true,
+        "trello": true,
+        "equella": true,
+        "teamDynamix": true,
+        "teamDrive": true,
+        "microsoft": true,
+        "workDay": true,
+        "staffHub": true,
+        "canvas": true,
+        "employeeDirectory": true
+        }, {
+        "name": "Albree Joy",
+        "email": "wak13001@byui.edu",
+        "brightspace": true,
+        "trello": true,
+        "equella": true,
+        "teamDynamix": true,
+        "teamDrive": true,
+        "microsoft": true,
+        "workDay": true,
+        "staffHub": true,
+        "canvas": true,
+        "employeeDirectory": true
+        }, {
+        "name": "Taylor Wanlass",
+        "email": "wan15005@byui.edu",
+        "brightspace": true,
+        "trello": true,
+        "equella": true,
+        "teamDynamix": true,
+        "teamDrive": true,
+        "microsoft": true,
+        "workDay": true,
+        "staffHub": true,
+        "canvas": true,
+        "employeeDirectory": true
+        }, {
+        "name": "Adrian Whetten",
+        "email": "whe15017@byui.edu",
+        "brightspace": true,
+        "trello": true,
+        "equella": true,
+        "teamDynamix": true,
+        "teamDrive": true,
+        "microsoft": true,
+        "workDay": true,
+        "staffHub": true,
+        "canvas": true,
+        "employeeDirectory": true
+        }, {
+        "name": "Shayla Salazar",
+        "email": "max16011@byui.edu",
+        "brightspace": true,
+        "trello": true,
+        "equella": true,
+        "teamDynamix": true,
+        "teamDrive": true,
+        "microsoft": true,
+        "workDay": true,
+        "staffHub": true,
+        "canvas": true,
+        "employeeDirectory": true
+        }, {
+        "name": "Brooklyn Cook",
+        "email": "coo15018@byui.edu",
+        "brightspace": false,
+        "trello": true,
+        "equella": true,
+        "teamDynamix": false,
+        "teamDrive": true,
+        "microsoft": true,
+        "workDay": false,
+        "staffHub": true,
+        "canvas": false,
+        "employeeDirectory": true
+        }, {
+        "name": "Taylor Beavers",
+        "email": "bea15001@byui.edu",
+        "brightspace": true,
+        "trello": true,
+        "equella": true,
+        "teamDynamix": true,
+        "teamDrive": true,
+        "microsoft": true,
+        "workDay": true,
+        "staffHub": true,
+        "canvas": true,
+        "employeeDirectory": true
+        }, {
+        "name": "Kaden Heaton",
+        "email": "hea11006@byui.edu",
+        "brightspace": true,
+        "trello": true,
+        "equella": true,
+        "teamDynamix": true,
+        "teamDrive": true,
+        "microsoft": true,
+        "workDay": true,
+        "staffHub": true,
+        "canvas": true,
+        "employeeDirectory": true
+        }, {
+        "name": "Rachel James",
+        "email": "jam17003@byui.edu",
+        "brightspace": true,
+        "trello": true,
+        "equella": true,
+        "teamDynamix": true,
+        "teamDrive": true,
+        "microsoft": true,
+        "workDay": true,
+        "staffHub": true,
+        "canvas": true,
+        "employeeDirectory": true
+        }, {
+        "name": "Brooke Ericson",
+        "email": "eri11006@byui.edu",
+        "brightspace": true,
+        "trello": true,
+        "equella": true,
+        "teamDynamix": true,
+        "teamDrive": true,
+        "microsoft": true,
+        "workDay": true,
+        "staffHub": true,
+        "canvas": true,
+        "employeeDirectory": true
+        }, {
+        "name": "John Soon",
+        "email": "soo16001@byui.edu",
+        "brightspace": true,
+        "trello": true,
+        "equella": true,
+        "teamDynamix": true,
+        "teamDrive": true,
+        "microsoft": true,
+        "workDay": true,
+        "staffHub": true,
+        "canvas": true,
+        "employeeDirectory": true
+        }, {
+        "name": "Seth Farr",
+        "email": "far16016@byui.edu",
+        "brightspace": true,
+        "trello": true,
+        "equella": true,
+        "teamDynamix": true,
+        "teamDrive": true,
+        "microsoft": true,
+        "workDay": true,
+        "staffHub": true,
+        "canvas": true,
+        "employeeDirectory": true
+        }, {
+        "name": "Emma Stevenson",
+        "email": "hou51003@byui.edu",
+        "brightspace": true,
+        "trello": true,
+        "equella": true,
+        "teamDynamix": true,
+        "teamDrive": true,
+        "microsoft": true,
+        "workDay": true,
+        "staffHub": true,
+        "canvas": true,
+        "employeeDirectory": true
+        }, {
+        "name": "Cal Wilson",
+        "email": "wil13034@byui.edu",
+        "brightspace": true,
+        "trello": true,
+        "equella": true,
+        "teamDynamix": true,
+        "teamDrive": true,
+        "microsoft": true,
+        "workDay": true,
+        "staffHub": true,
+        "canvas": true,
+        "employeeDirectory": true
+        }, {
+        "name": "Casey Streeter",
+        "email": "str14028@byui.edu",
+        "brightspace": true,
+        "trello": true,
+        "equella": true,
+        "teamDynamix": true,
+        "teamDrive": true,
+        "microsoft": true,
+        "workDay": true,
+        "staffHub": true,
+        "canvas": true,
+        "employeeDirectory": true
+        }, {
+        "name": "Julie James",
+        "email": "jam15006@byui.edu",
+        "brightspace": true,
+        "trello": true,
+        "equella": true,
+        "teamDynamix": true,
+        "teamDrive": true,
+        "microsoft": true,
+        "workDay": true,
+        "staffHub": true,
+        "canvas": true,
+        "employeeDirectory": true
+        }, {
+        "name": "Welechele Shabandoje",
+        "email": "sha12039@byui.edu",
+        "brightspace": true,
+        "trello": true,
+        "equella": true,
+        "teamDynamix": true,
+        "teamDrive": true,
+        "microsoft": true,
+        "workDay": true,
+        "staffHub": true,
+        "canvas": true,
+        "employeeDirectory": true
+        }, {
+        "name": "Mahonri Saldana",
+        "email": "ste16023@byui.edu",
+        "brightspace": true,
+        "trello": true,
+        "equella": true,
+        "teamDynamix": true,
+        "teamDrive": true,
+        "microsoft": true,
+        "workDay": true,
+        "staffHub": true,
+        "canvas": true,
+        "employeeDirectory": true
+        }, {
+        "name": "James Rees",
+        "email": "ree15011@byui.edu",
+        "brightspace": true,
+        "trello": true,
+        "equella": true,
+        "teamDynamix": true,
+        "teamDrive": true,
+        "microsoft": true,
+        "workDay": true,
+        "staffHub": true,
+        "canvas": true,
+        "employeeDirectory": true
+        }, {
+        "name": "Corey Moore",
+        "email": "mooreco@byui.edu",
+        "brightspace": true,
+        "trello": true,
+        "equella": true,
+        "teamDynamix": true,
+        "teamDrive": true,
+        "microsoft": true,
+        "workDay": true,
+        "staffHub": true,
+        "canvas": true,
+        "employeeDirectory": true
+        }, {
+        "name": "Ian Caloobanan",
+        "email": "cal15034@byui.edu",
+        "brightspace": true,
+        "trello": true,
+        "equella": true,
+        "teamDynamix": true,
+        "teamDrive": true,
+        "microsoft": true,
+        "workDay": true,
+        "staffHub": true,
+        "canvas": true,
+        "employeeDirectory": true
+        }, {
+        "name": "Sierra Gervasi",
+        "email": "ger14005@byui.edu",
+        "brightspace": true,
+        "trello": true,
+        "equella": true,
+        "teamDynamix": true,
+        "teamDrive": true,
+        "microsoft": true,
+        "workDay": true,
+        "staffHub": true,
+        "canvas": true,
+        "employeeDirectory": true
+        }, {
+        "name": "Logan Jenkins",
+        "email": "jen12060@byui.edu",
+        "brightspace": true,
+        "trello": true,
+        "equella": true,
+        "teamDynamix": true,
+        "teamDrive": true,
+        "microsoft": true,
+        "workDay": true,
+        "staffHub": true,
+        "canvas": true,
+        "employeeDirectory": true
+        }, {
+        "name": "Megan Havens",
+        "email": "hav17001@byui.edu",
+        "brightspace": true,
+        "trello": true,
+        "equella": true,
+        "teamDynamix": true,
+        "teamDrive": true,
+        "microsoft": true,
+        "workDay": true,
+        "staffHub": true,
+        "canvas": true,
+        "employeeDirectory": true
+        }, {
+        "name": "Sophia Beyer",
+        "email": "bey16003@byui.edu",
+        "brightspace": true,
+        "trello": true,
+        "equella": true,
+        "teamDynamix": true,
+        "teamDrive": true,
+        "microsoft": true,
+        "workDay": true,
+        "staffHub": true,
+        "canvas": true,
+        "employeeDirectory": true
+        }
+    ]
 /*
-"wd" 
-"trel"
-"equ"
-"tdy"
-"tdr"
-"micr"
-"bs" 
-"sh" 
-"can"
-"ed" */
+    "wd" 
+    "trel"
+    "equ"
+    "tdy"
+    "tdr"
+    "micr"
+    "bs" 
+    "sh" 
+    "can"
+    "ed" */
