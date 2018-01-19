367adbdc81bbfca32e04c7c60eb2b45de59d9f4a
Zoe <min16009@byui.edu>
2018-01-18 17:31:43 -0700

    FF

--- main.js
+++ main.js
@@ -601,7 +601,7 @@
         var comment = {
             CommentIn: cmessage
         };
-        if (comment != "") {
+        if (cmessage != "") {
             ref.child(date).update(comment);
             document.getElementById('comment').innerHTML = "";
         } else {
@@ -629,7 +629,7 @@
         var comment = {
             CommentOut: cmessage
         };
-        if (comment != "") {
+        if (cmessage != "") {
             ref.child(date).update(comment);
             document.getElementById('comment').innerHTML = "";
         } else {
