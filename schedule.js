30ae60f3c6d781e5b30d0c08dd00c410c612c50d
minzoe <min16009@byui.edu>
2018-02-01 16:17:01 -0700

    Change Schedules

--- schedule.js
+++ schedule.js
@@ -1,48 +1,64 @@
 (function () {
-        //------------------ Connect to Firebase -----------------------//
-        const config = {
-            apiKey: "AIzaSyA_I75-CU5_GlNP1QSKvvH8nbYVkaAUgNA",
-            authDomain: "techopsportal.firebaseapp.com",
-            databaseURL: "https://techopsportal.firebaseio.com",
-            projectId: "techopsportal",
-            storageBucket: "techopsportal.appspot.com",
-            messagingSenderId: "265124430634"
-        };
-        firebase.initializeApp(config);
+    //------------------ Connect to Firebase -----------------------//
+    const config = {
+        apiKey: "AIzaSyA_I75-CU5_GlNP1QSKvvH8nbYVkaAUgNA",
+        authDomain: "techopsportal.firebaseapp.com",
+        databaseURL: "https://techopsportal.firebaseio.com",
+        projectId: "techopsportal",
+        storageBucket: "techopsportal.appspot.com",
+        messagingSenderId: "265124430634"
+    };
+    firebase.initializeApp(config);
 
-        // Function to check which team
-        var name;
-        var user = firebase.auth().currentUser;
-        user.providerData.forEach(function (profile) {
+    // Check if Logged In
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if (firebaseUser) {
+            // Function to check which team
+            var name;
+            var user = firebase.auth().currentUser;
+            user.providerData.forEach(function (profile) {
                 name = profile.displayName;
-                firebase.database.re('users').child(user).on('value', snap => {
-                        var titles;
-                        var shot = snap.val();
-                        for (titles in shot) {
-                            if (titles == 'Admin') {
-                                var frame1 = document.createElement('iframe');
-                                frame1.setAttribute('src', 'https://docs.google.com/spreadsheets/d/1mY6vvcCC7ptSYrlnoFrhwgWh4DMqGwQNdcgtjUz4f9M/edit#gid=266358356');
-                                frame1.setAttribute('class', 'iframes');
-
-                                var frame2 = document.createElement('iframe');
-                                frame2.setAttribute('scr', 'https://docs.google.com/spreadsheets/d/1ge7AlgKH_0nkvmbZ_BxWiP6WfAg2rynA1ccXMFei22k/edit#gid=1630866338');
-                                frame2.setAttribute('class', 'iframes');
-
-
-                                var frame3 = document.createElement('iframe');
-                                frame3.setAttribute('src', 'https://docs.google.com/spreadsheets/d/1BMVKAqfiPspOLZy4OFPHdccCWWJVtkHMrLoNobFCkTI/edit#gid=2021360543');
-                                frame3.setAttribute('class', 'iframes');
+                firebase.database().ref('users').child(name).on('value', snap => {
+                    var titles;
+                    var shot = snap.val();
+                    for (titles in shot) {
+                        if (titles == 'Admin') {
+                            document.getElementById('team-dropdown').classList.remove('hide');
+                            break;
+                        } else if (titles == 'Team') {
+                            document.getElementById('scheduleFrame').classList.remove('hide');
+                            if (shot[titles] == 'tech') {
+                                document.getElementById('scheduleFrame').setAttribute('src', 'https://docs.google.com/spreadsheets/d/1mY6vvcCC7ptSYrlnoFrhwgWh4DMqGwQNdcgtjUz4f9M/edit?usp=sharing');
+                            } else if (shot[titles] == 'canvas') {
+                                document.getElementById('scheduleFrame').setAttribute('src', 'https://docs.google.com/spreadsheets/d/1ge7AlgKH_0nkvmbZ_BxWiP6WfAg2rynA1ccXMFei22k/edit?usp=sharing');
+                            } else if (shot[titles] == 'transcript') {
+                                document.getElementById('scheduleFrame').setAttribute('src', 'https://docs.google.com/spreadsheets/d/1BMVKAqfiPspOLZy4OFPHdccCWWJVtkHMrLoNobFCkTI/edit?usp=sharing');
                             }
-
                         }
-                        if (shot[titles] == 'tech') {
-                            document.getElementById('scheduleFrame').setAttribute('src', 'https://docs.google.com/spreadsheets/d/1mY6vvcCC7ptSYrlnoFrhwgWh4DMqGwQNdcgtjUz4f9M/edit#gid=266358356');
-                        } else if (shot[titles] == 'canvas') {
-                            document.getElementById('scheduleFrame').setAttribute('src', 'https://docs.google.com/spreadsheets/d/1ge7AlgKH_0nkvmbZ_BxWiP6WfAg2rynA1ccXMFei22k/edit#gid=1630866338');
-                        } else if (shot[titles] == 'transcript') {
-                            document.getElementById('scheduleFrame').setAttribute('src', 'https://docs.google.com/spreadsheets/d/1BMVKAqfiPspOLZy4OFPHdccCWWJVtkHMrLoNobFCkTI/edit#gid=2021360543');
-                        } else if ()
+
                     }
+
                 });
-        });
+            });
+
+        } else {
+            window.location.replace("index.html");
+        }
+    });
 }());
+
+
+function showTeam(team) {
+    var frame = document.getElementById('scheduleFrame');
+    frame.setAttribute('src', '');
+    if (team == 'tech') {
+        console.log('Tech team')
+        frame.setAttribute('src', 'https://docs.google.com/spreadsheets/d/1mY6vvcCC7ptSYrlnoFrhwgWh4DMqGwQNdcgtjUz4f9M/edit?usp=sharing');
+    } else if (team == 'canvas') {
+        console.log('Canvas team')
+        frame.setAttribute('src', 'https://docs.google.com/spreadsheets/d/1ge7AlgKH_0nkvmbZ_BxWiP6WfAg2rynA1ccXMFei22k/edit?usp=sharing');
+    } else if (team == 'transcript') {
+        console.log('Trans team')
+        frame.setAttribute('src', 'https://docs.google.com/spreadsheets/d/1BMVKAqfiPspOLZy4OFPHdccCWWJVtkHMrLoNobFCkTI/edit?usp=sharing');
+    }
+}
