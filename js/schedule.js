699eeb76fc0c8183dd3c91acd6dc46f6674188cf
minzoe <min16009@byui.edu>
2018-02-13 14:04:14 -0700

    Admin Page

--- js/schedule.js
+++ js/schedule.js
@@ -47,16 +47,16 @@
 }());
 
 function showTeam(team) {
-    var frame = document.getElementById('scheduleFrame');
-    frame.setAttribute('src', '');
-    if (team == 'tech') {
-        console.log('Tech team')
-        frame.setAttribute('src', 'https://docs.google.com/spreadsheets/d/1mY6vvcCC7ptSYrlnoFrhwgWh4DMqGwQNdcgtjUz4f9M/edit?usp=sharing');
-    } else if (team == 'canvas') {
-        console.log('Canvas team')
-        frame.setAttribute('src', 'https://docs.google.com/spreadsheets/d/1ge7AlgKH_0nkvmbZ_BxWiP6WfAg2rynA1ccXMFei22k/edit?usp=sharing');
-    } else if (team == 'transcript') {
-        console.log('Trans team')
-        frame.setAttribute('src', 'https://docs.google.com/spreadsheets/d/1BMVKAqfiPspOLZy4OFPHdccCWWJVtkHMrLoNobFCkTI/edit?usp=sharing');
+        var frame = document.getElementById('scheduleFrame');
+        frame.setAttribute('src', '');
+        if (team == 'tech') {
+            console.log('Tech team')
+            frame.setAttribute('src', 'https://docs.google.com/spreadsheets/d/1mY6vvcCC7ptSYrlnoFrhwgWh4DMqGwQNdcgtjUz4f9M/edit?usp=sharing');
+        } else if (team == 'canvas') {
+            console.log('Canvas team')
+            frame.setAttribute('src', 'https://docs.google.com/spreadsheets/d/1ge7AlgKH_0nkvmbZ_BxWiP6WfAg2rynA1ccXMFei22k/edit?usp=sharing');
+        } else if (team == 'transcript') {
+            console.log('Trans team')
+            frame.setAttribute('src', 'https://docs.google.com/spreadsheets/d/1BMVKAqfiPspOLZy4OFPHdccCWWJVtkHMrLoNobFCkTI/edit?usp=sharing');
+        }
     }
-}
