d98537a8029fe8dcfecf42dbba32e5ec74fa469d
minzoe <min16009@byui.edu>
2018-02-15 13:17:37 -0700

    Add Admin Link

--- js/home.js
+++ js/home.js
@@ -11,6 +11,29 @@
 
     firebase.auth().onAuthStateChanged(firebaseUser => {
         if (firebaseUser) {
+            var name;
+            var user = firebase.auth().currentUser;
+            user.providerData.forEach(function (profile) {
+                name = profile.displayName;
+                firebase.database().ref('users').child(name).on('value', snap => {
+                    var titles;
+                    var shot = snap.val();
+                    for (titles in shot) {
+                        if (titles == 'Admin') {
+                            //Load Admin Link
+                            var ul = document.getElementById('sidenav');
+                            var li = document.createElement('li');
+                            var a = document.createElement('a');
+                            a.setAttribute('href', 'admin.html');
+                            var t = document.createTextNode('Admin');
+                            a.appendChild(t);
+                            li.appendChild(a);
+                            var ref = ul.lastChild;
+                            ref.parentNode.insertBefore(li, ref.nextSibling);
+                        }
+                    }
+                });
+            });
 
             var loggedinUser = firebase.auth().currentUser;
             var user;
