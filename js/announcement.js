cf215aa542db5be1834f69b0a9c8d08611ebc036
Zoe <min16009@byui.edu>
2018-02-21 13:10:40 -0700

    Announcements

--- js/announcement.js
+++ js/announcement.js
@@ -1,17 +1,16 @@
-(function () {
     // Firebase
     const config = {
-        apiKey: "AIzaSyA_I75-CU5_GlNP1QSKvvH8nbYVkaAUgNA",
-        authDomain: "techopsportal.firebaseapp.com",
-        databaseURL: "https://techopsportal.firebaseio.com",
-        projectId: "techopsportal",
-        storageBucket: "techopsportal.appspot.com",
-        messagingSenderId: "265124430634"
+        apiKey: "AIzaSyA_I75-CU5_GlNP1QSKvvH8nbYVkaAUgNA"
+        , authDomain: "techopsportal.firebaseapp.com"
+        , databaseURL: "https://techopsportal.firebaseio.com"
+        , projectId: "techopsportal"
+        , storageBucket: "techopsportal.appspot.com"
+        , messagingSenderId: "265124430634"
     };
     firebase.initializeApp(config);
-
     firebase.auth().onAuthStateChanged(firebaseUser => {
         if (firebaseUser) {
+            var db = firebase.database().ref('announcements');
             var name;
             var user = firebase.auth().currentUser;
             user.providerData.forEach(function (profile) {
@@ -35,7 +34,6 @@
                     }
                 });
             });
-
             var db = firebase.database().ref('announcements');
             db.on('value', snap => {
                 console.log(snap.val())
@@ -43,35 +41,35 @@
 
             function sendAnnouncementToFirebase(title, message, image, text, link, date) {
                 var data = {
-                    "title": title,
-                    "message": message,
-                    "image": image,
-                    "text": text,
-                    "link": link,
-                    "date": date
+                    "title": title
+                    , "message": message
+                    , "image": image
+                    , "text": text
+                    , "link": link
+                    , "date": date
                 }
                 var key = db.push().key;
                 db.push(data);
                 modal.style.display = "none";
             }
-
             // Display Announcements
             db.on('value', snap => {
+                var keys = Object.keys(snap.val());
                 var html = "";
-                snap.forEach(function (snapshot) {
-                    var childKey = snapshot.key;
-                    var childData = snapshot.val();
-
+                for (var i = 0; i < keys.length; i++) {
+                    var childKey = snap.key;
+                    var childData = snap.val()[keys[i]];
+                    if (childData.urgency == true) {
+                        var urgent = "<strong style='color: red' font-family: 'Impact'>URGENT</strong>";
+                    }
+                    else var urgent = "";
                     if (childData != "null") {
-                        html = "<div class='item'><img src='" + childData.image + "'><h2>" + childData.title + "</h2><p>" + childData.message + "</p><a href='" + childData.link + "' target='_blank'>" + childData.text + "</a></div>" + html;
-                    };
-
-                });
-
+                        console.log(keys[i])
+                        html += "<div class='item'>" + urgent + "<h2>" + childData.title + "</h2><p>" + childData.message + "</p><input style='max-width: 50%;' type='button' onclick=\"deleteAnnouncement('" + keys[i] + "')\" value='Delete'></div>";
+                    }
+                }
                 document.getElementById('display').innerHTML = html;
-
             });
-
             // Delete Announcements
             db.on('value', snap => {
                 snap.forEach(function (snapshot) {
@@ -88,106 +86,70 @@
                         date = '0' + date;
                     }
                     var today = d.getFullYear() + "-" + (month) + "-" + date;
-
-                    if (firebasedate < today) {
-                        db.child(childKey).set(null);
-                    };
+                    //                    if (firebasedate < today) {
+                    //                        db.child(childKey).set(null);
+                    //                    };
                 });
             });
-
             // Get the modal
             var modal = document.getElementById('myModal');
-
             // Get the button that opens the modal
             var btn = document.getElementById("myBtn");
-
             // Get the <span> element that closes the modal
             var span = document.getElementsByClassName("close")[0];
-
             // When the user clicks on the button, open the modal 
             btn.onclick = function () {
-                modal.style.display = "block";
-            }
-
-            // When the user clicks on <span> (x), close the modal
+                    modal.style.display = "block";
+                }
+                // When the user clicks on <span> (x), close the modal
             span.onclick = function () {
-                modal.style.display = "none";
-                document.getElementById('title').value = "";
-                document.getElementById('image').value = "";
-                document.getElementById('text').value = "";
-                document.getElementById('link').value = "";
-                document.getElementById('date').value = "";
-                document.getElementById('imagecheck').checked = false;
-                document.getElementById('linkcheck').checked = false;
-                isChecked();
-                tinymce.get('message').setContent("");
-            }
-
-            // When the user clicks anywhere outside of the modal, close it
+                    modal.style.display = "none";
+                    document.getElementById('title').value = "";
+                    document.getElementById('message').value = "";
+                    document.getElementById('urgency').value = "";
+                    document.getElementById('date').value = "";
+                }
+                // When the user clicks anywhere outside of the modal, close it
             window.onclick = function (event) {
                 if (event.target == modal) {
                     modal.style.display = "none";
                     document.getElementById('title').value = "";
-                    document.getElementById('image').value = "";
-                    document.getElementById('text').value = "";
-                    document.getElementById('link').value = "";
+                    document.getElementById('message').value = "";
+                    document.getElementById('urgency').value = "";
                     document.getElementById('date').value = "";
-                    document.getElementById('imagecheck').checked = false;
-                    document.getElementById('linkcheck').checked = false;
-                    isChecked();
-                    tinymce.get('message').setContent("");
                 }
             }
-
-            // Is the Checkbox checked
-            function isChecked() {
-                var imgcheck = document.getElementById('imagecheck');
-                var selectImage = document.getElementById('selectImage');
-                if (imgcheck.checked) {
-                    selectImage.classList.remove('hide');
-                } else {
-                    selectImage.classList.add('hide');
-                }
-
-                var linkcheck = document.getElementById('linkcheck');
-                var addLink = document.getElementById('addLink');
-                if (linkcheck.checked) {
-                    addLink.classList.remove('hide');
-                } else {
-                    addLink.classList.add('hide');
-                }
-            }
-
-            // Submit the form
-            document.getElementById('submitAnnounce').addEventListener('click', e => {
-                var title = document.getElementById('title').value;
-                var message = tinymce.get('message').getContent();
-                var image = document.getElementById('image').value;
-                var text = document.getElementById('text').value;
-                var link = document.getElementById('link').value;
-                var date = document.getElementById('date').value;
-
-                sendAnnouncementToFirebase(title, message, image, text, link, date);
-
-                document.getElementById('title').value = "";
-                document.getElementById('image').value = "";
-                document.getElementById('text').value = "";
-                document.getElementById('link').value = "";
-                document.getElementById('date').value = "";
-                document.getElementById('imagecheck').checked = false;
-                document.getElementById('linkcheck').checked = false;
-                isChecked();
-                tinymce.get('message').setContent("");
-            });
-
-            // WYSIWYG
-            tinymce.init({
-                selector: 'textarea'
-            });
-        } else {
+        }
+        else {
             // User is signed out.
             window.location.replace("index.html");
             // ...
         }
     });
-}());
+
+    function deleteAnnouncement(toDelete) {
+        if (confirm('Are you sure you want to delete this announcement?')) {
+            firebase.database().ref('announcements/' + toDelete).remove();
+        }
+        else {
+            return;
+        }
+    }
+
+    function sendAnnouncementToFirebase() {
+        var modal = document.getElementById('myModal');
+        var currentDate = new Date();
+        var db = firebase.database().ref('announcements/' + currentDate);
+        var title = document.getElementById("title").value;
+        var message = document.getElementById("message").value;
+        var urgency = document.getElementById("urgency").checked;
+        var date = document.getElementById("date").value;
+        var data = {
+            "title": title
+            , "message": message
+            , "urgency": urgency
+            , "expirationDate": date
+        }
+        db.update(data);
+        modal.style.display = "none";
+    }
