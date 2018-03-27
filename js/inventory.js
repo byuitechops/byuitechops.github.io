0eb8dbe2b86718d00b8ffbdcd7bfc05afaf10553
brookericson <eri11006@byui.edu>
2018-03-27 12:11:46 -0600

    updated with shopping cart and inventory

---
+++ js/inventory.js
@@ -0,0 +1,261 @@
+// Connect to firebase
+const config = {
+    apiKey: "AIzaSyA_I75-CU5_GlNP1QSKvvH8nbYVkaAUgNA",
+    authDomain: "techopsportal.firebaseapp.com",
+    databaseURL: "https://techopsportal.firebaseio.com",
+    projectId: "techopsportal",
+    storageBucket: "techopsportal.appspot.com",
+    messagingSenderId: "265124430634"
+};
+
+//initialize Firebase
+firebase.initializeApp(config);
+
+//Make sure the user is logged in, if so call createTable function
+firebase.auth().onAuthStateChanged(firebaseUser => {
+    if (firebaseUser) {
+        // If logged in do this
+        user = firebase.auth().currentUser;
+        user.providerData.forEach(function (profile) {
+            name = profile.displayName;
+            firebase.database().ref('users').child(name).on('value', snap => {
+                var titles;
+                var shot = snap.val();
+                for (titles in shot) {
+                    if (titles == 'Admin') {
+                        if (shot[titles] == true) {
+                            //Load Admin Link
+                            document.getElementById('adminlink').classList.remove('hide');
+                        }
+                    }
+//                    else {
+//                        window.location.replace("home.html");
+//                    }
+                }
+            });
+        });
+        createTable();
+    } else {
+        window.location.replace("index.html");
+    }
+});
+
+
+// retrieves inventory items from firebase and uses items to create shopping cart table
+function createTable() {
+    var id = 1; //id number used to identify the number of each row of the table
+
+    var table = document.getElementById("inventory-table");
+    table.innerHTML = ''; //clear any existing cells in the inventory table
+    var row = table.insertRow(-1);  //insert row containing table column headings
+    row.insertCell(0).innerHTML = "<b>Item</b>";
+    var cell2 = row.insertCell(1).innerHTML = "<b>Price</b>";
+    var cell3 = row.insertCell(2).innerHTML = "<b>Stock</b>";
+    var cell4 = row.insertCell(3);
+    
+    var query = firebase.database().ref("inventory/items").orderByKey(); //get inventory items from firebase
+    query.once("value")
+        .then(function (snapshot) { //loop through each inventory item
+            snapshot.forEach(function (childSnapshot) {
+                var key = childSnapshot.key; //gets inventory item name
+                var childData = childSnapshot.val(); //gets data associated with item
+                var count = childData.count;
+                var price = parseFloat(childData.price).toFixed(2);
+
+                //create new row with four cell into table
+                var row = table.insertRow(-1);
+                var cell1 = row.insertCell(0);
+                var cell2 = row.insertCell(1);
+                var cell3 = row.insertCell(2);
+                var cell4 = row.insertCell(3);
+
+                //insert name, price, and quanity of item into cells
+                cell1.innerHTML = key;
+                cell2.innerHTML = price;
+                cell3.innerHTML = count;
+     
+                //give each cell an id
+                cell1.id = "item" + id;
+                cell2.id = "price" + id;
+                cell3.id = "count" + id;
+                
+                //create the edit button by appending a span to cell 4
+                var updateButton = cell4.appendChild(document.createElement("span"));
+                updateButton.innerHTML = "Edit";
+                updateButton.style.cursor = "pointer";
+                updateButton.style.color = "#0076c6";
+                
+                //add click eventlistener to span that calls displayUpdateModal function
+                updateButton.addEventListener("click", function () {
+                    displayUpdateModal(key);
+                });
+              
+                //create the delete button by appending another span to cell 4
+                var deleteButton = cell4.appendChild(document.createElement("span"));
+                deleteButton.innerHTML = "&#x2715;";
+                deleteButton.style.cursor = "pointer";
+                
+                //add click eventlistener to span that calls delete function
+                deleteButton.addEventListener("click", function () {
+                    deleteItem(key);
+                });
+                
+                id += 1; //increment id by 1
+            });
+        
+            //create a the last row in table to contain input fields for new inventory items
+            var row = table.insertRow(-1);
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            var cell3 = row.insertCell(2);
+            var cell4 = row.insertCell(3);
+        
+            //create three input elements
+            var input1 = document.createElement("input");
+            var input2 = document.createElement("input");
+            var input3 = document.createElement("input");
+        
+            //give each input a new id
+            input1.id = "input1";
+            input2.id = "input2";
+            input3.id = "input3";
+        
+            //set the type attribute on each input
+            input1.setAttribute("type", "text");
+            input2.setAttribute("type", "number");
+            input3.setAttribute("type", "number");
+
+            //set the min range and step for input 2 and 3 to zero
+            input2.step = 0.01;
+            input2.min = 0;
+            input3.min = 0;
+        
+            //set the name for each input
+            input1.name = "itemName";
+            input2.name = "price";
+            input3.name = "quantity";
+        
+            //append each input to respective cell within new row
+            cell1.appendChild(input1);
+            cell2.appendChild(input2);
+            cell3.appendChild(input3);
+        
+            //append button to cell 4
+            var addButton = cell4.appendChild(document.createElement("button"));
+            addButton.innerHTML = "Add";
+            addButton.style.fontWeight = "bold";
+        
+            //add click eventListener to call createItem function
+            addButton.addEventListener("click", createItem);
+            
+        });
+}
+
+
+//displays modal allowing user to update a selected item
+function displayUpdateModal(item) {
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementById("close");
+
+    // Open the modal 
+    modal.style.display = "block";
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    //display the name of item to be edited in modal
+    document.getElementById("name").innerHTML = item;
+
+    //retrieve the price and count from firebase for selected item
+    firebase.database().ref('/inventory/items/' + item).once('value').then(function (snapshot) {
+        var itemData = snapshot.val(); //data for selected item
+        var itemPrice = parseFloat(itemData.price).toFixed(2);
+        var itemCount = itemData.count;
+
+        //display the current price and quantity of item to user in input fields
+        document.getElementById("price").value = itemPrice;
+        document.getElementById("quantity").value = itemCount;
+    });
+}
+
+//edits and updates selected item in firebase
+function editItem() {
+    
+    // get item, name, price, and quantity
+    var item = document.getElementById("name").innerHTML;
+    var price = parseFloat((document.getElementById("price").value)).toFixed(2); 
+    var quantity = document.getElementById("quantity").value;
+    
+    //check that input fields are not empty, if not update new data for item to firebase
+    if (item != '' && price != '' && quantity != '') { 
+        newData = {
+            "count": quantity,
+            "price": price
+        }
+        updates = {};
+        updates['/inventory/items/' + item] = newData;
+        firebase.database().ref().update(updates);
+        createTable(); //call createTable to re-render updated table on the user interface
+    }
+}
+
+//deletes selected item
+function deleteItem(item) {
+    //display confirmation pop-up box prompting user to confirm the deletion
+    var a = confirm("Are you sure you would like to delete" + item + "?");
+    
+    //if the user confirms the deletion, delete the selected item from firebase
+    if (a) {
+        newData = {
+            "count": null,
+            "price": null
+        }
+        updates = {};
+        updates['/inventory/items/' + item] = newData;
+        firebase.database().ref().update(updates);
+        createTable(); //call createTable to re-render updated table on the user interface 
+    }
+}
+
+//creates a new item and writes new item to firebase
+function createItem() {
+    
+    //get the name, price, and quantity of new item from input values
+    var item = document.getElementById("input1").value;
+    var price = parseFloat((document.getElementById("input2").value)).toFixed(2);
+    var quantity = document.getElementById("input3").value;
+    
+    //check that each input has a value, if none of the inputs are empty, write the new item to firebase
+    if (item != '' && price != '' && quantity != '') {
+        newData = {
+            "count": quantity,
+            "price": price
+        }
+        updates = {};
+        updates['/inventory/items/' + item] = newData;
+        firebase.database().ref().update(updates);
+        createTable();
+    }
+    else if (item == ''){ //if item input is empty display message to enter item name 
+        document.getElementById("warning").innerHTML = "Please enter an item name";
+    }
+    else if (price == ''){//if item input is empty display message to enter price 
+        document.getElementById("warning").innerHTML = "Please enter a price for the new item";
+    }
+    else if (quantity == ''){//if item input is empty display message to enter quantity
+        document.getElementById("warning").innerHTML = "Please enter the number of items in stock";
+    }
+}
+
---
+++ js/shoppingCart.js
@@ -0,0 +1,243 @@
+// Connect to firebase
+const config = {
+    apiKey: "AIzaSyA_I75-CU5_GlNP1QSKvvH8nbYVkaAUgNA",
+    authDomain: "techopsportal.firebaseapp.com",
+    databaseURL: "https://techopsportal.firebaseio.com",
+    projectId: "techopsportal",
+    storageBucket: "techopsportal.appspot.com",
+    messagingSenderId: "265124430634"
+};
+firebase.initializeApp(config);
+
+// Check that user is logged in, if so run createTable function, if not redirect to index.html
+firebase.auth().onAuthStateChanged(firebaseUser => {
+    if (firebaseUser) {
+        // If logged in do this
+        user = firebase.auth().currentUser;
+        user.providerData.forEach(function (profile) {
+            name = profile.displayName;
+            firebase.database().ref('users').child(name).on('value', snap => {
+                var titles;
+                var shot = snap.val();
+                for (titles in shot) {
+                    if (titles == 'Admin') {
+                        if (shot[titles] == true) {
+                            //Load Admin Link
+                            document.getElementById('adminlink').classList.remove('hide');
+                        }
+                    }
+                }
+            });
+        });
+        createTable();
+    } else {
+        window.location.replace("index.html");
+    }
+});
+
+
+// use inventory items stored in firebase to create rows in shopping cart table
+function createTable() {
+
+    var table = document.getElementById("shopping-cart");
+    var id = 1; //id used to identify each row
+
+    // reads and loops through inventory items in firebase
+    var query = firebase.database().ref("inventory/items").orderByKey();
+    query.once("value")
+        .then(function (snapshot) {
+            snapshot.forEach(function (childSnapshot) {
+                
+                var key = childSnapshot.key; // item name
+                var childData = childSnapshot.val(); // data for item
+                var count = childData.count; 
+                var price = parseFloat(childData.price).toFixed(2);
+
+                // create row with four cells
+                var row = table.insertRow(-1);
+                var cell1 = row.insertCell(0);
+                var cell2 = row.insertCell(1);
+                var cell3 = row.insertCell(2);
+                var cell4 = row.insertCell(3);
+
+                // insert item name, price, and count into cells
+                cell1.innerHTML = key;
+                cell2.innerHTML = price;
+                cell3.innerHTML = count;
+
+                // give each cell an id
+                cell1.id = "item" + id;
+                cell2.id = "price" + id;
+                cell3.id = "count" + id;
+                
+                // create an input element and append to cell 4
+                var input = cell4.appendChild(document.createElement("input"));
+                input.setAttribute("type", "number");
+                input.setAttribute("max", count);
+                input.setAttribute("min", 0);
+                input.setAttribute("placeholder", 0);
+                input.setAttribute("id", "input" + id);
+                id += 1;
+                
+                // add click event listener to input to call calculateTotal function
+                input.addEventListener("input", calculateTotal);
+            });
+        });
+}
+
+// calculate the total price of purchase
+function calculateTotal() {
+    var rows = document.getElementById("shopping-cart").rows.length - 1; // total length of rows in shopping cart
+    var total = 0;
+
+    for (var i = 1; i <= rows; i++) { // loop through each row
+        var count = document.getElementById("input" + i).value; // get quantity of item wanted by user
+        var price = document.getElementById("price" + i).innerHTML; // get price of item
+        var itemTotal = count * price; // find total price of item  
+        total += itemTotal; // add item total to over all total
+    }
+
+    document.getElementById("cart-total").innerHTML = "$" + (total.toFixed(2));
+}
+
+// opens the confirm purchase modal 
+function confirmPurchase() {
+
+    var message;
+    var rows = document.getElementById("shopping-cart").rows.length - 1;
+    var total = 0;
+
+    // loops through each row in the shopping cart table
+    for (var i = 1; i <= rows; i++) {
+        var count = document.getElementById("input" + i).value; // get quantity of item to be purchased
+
+        if (count > 0) { // if quantity of item to be purchased is greater than 0 add to list of items to be purchased in modal
+            var price = document.getElementById("price" + i).innerHTML; 
+            var itemTotal = count * price;
+            total += itemTotal; // add price of item to total cost
+            var item = document.getElementById("item" + i).innerHTML;
+            var li = document.createElement("LI");
+            li.innerHTML = "(" + count + ") " + item;
+            document.getElementById("modal-cart-items").appendChild(li);
+        }
+    }
+
+    document.getElementById("modal-cart-total").innerHTML = total.toFixed(2);    // display total cost of all items to modal
+
+    if (total > 0) { //if the total cost of all items is greater than 0 display modal
+
+        document.getElementById("notification").innerHTML = '';     // remove any message being displayed below the shopping cart table
+      
+        var modal = document.getElementById('myModal');   // Get the modal
+
+        var span = document.getElementById("close"); // Get the <span> element that closes the modal
+
+        modal.style.display = "block"; // Open the modal 
+
+        // When the user clicks on <span> (x), close the modal, clear item list, purchase total, warning message, and uncheck radio buttons
+        span.onclick = function () {
+            modal.style.display = "none";
+            document.getElementById('modal-cart-items').innerHTML = '';
+            document.getElementById('modal-cart-total').innerHTML = '';
+            document.getElementById('warning').innerHTML = '';
+            document.getElementById('payment-method-1').checked = false;
+            document.getElementById('payment-method-2').checked = false;
+        }
+
+        // When the user clicks anywhere outside of the modal, close it, clear item list, purchase total, warning message, and uncheck radio buttons
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+                document.getElementById('modal-cart-items').innerHTML = '';
+                document.getElementById('modal-cart-total').innerHTML = '';
+                document.getElementById('warning').innerHTML = '';
+                document.getElementById('payment-method-1').checked = false;
+                document.getElementById('payment-method-2').checked = false;
+            }
+        }
+    } else {  // if no items have been selected display warning message
+        message = "You must choose an item before checking out!";
+        var notification = document.getElementById("notification");
+        notification.innerHTML = message;
+        notification.style.color = "red";
+
+    }
+
+}
+
+// confirms purchase and updates firebase with count of item and venmo/cash payment totals
+function submitConfirmation() {
+    var radios = document.getElementsByName('payment-method');
+    var paymentMethod;
+    var message;
+    
+    // loop through radio buttons, if a button has been checked, set value of radio button to payment method
+    for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            paymentMethod = radios[i].value;
+        }
+    }
+
+    if (paymentMethod) {
+        var purchaseTotal = parseFloat(document.getElementById("modal-cart-total").innerHTML);
+        var newRunningTotal;
+        var newData;
+        var updates;
+        var rows = document.getElementById("shopping-cart").rows.length - 1;
+
+        //loop through shopping cart table rows
+        for (var i = 1; i <= rows; i++) {
+            var item = document.getElementById("item" + i).innerHTML; // get item name
+            var count = document.getElementById("input" + i).value; // get quantity of item to be purchased
+
+            if (count > 0) { // if quantity to be purchased is > 0, subtract the quantity of item from the number of items in stock currently
+                var currentCount = document.getElementById("count" + i).innerHTML;
+                var updatedCount = currentCount - count;
+
+                // Update firebase with an updated inventory count for item
+                firebase.database().ref('/inventory/items/'+ item).update({
+                    count: updatedCount
+                });
+                
+                // display the updated item count to the user in the shopping cart table
+                var currentCount = document.getElementById("count" + i).innerHTML = updatedCount;
+                document.getElementById("input" + i).value = ''; // clear the input fields
+            }
+        }
+
+        // get the running total for the selected payment method
+        firebase.database().ref("inventory/paymentTotals/" + paymentMethod).once('value').then(function (snapshot) {
+            var data = snapshot.val();
+            var runningTotal;
+            runningTotal = parseFloat(data.total);
+            newRunningTotal = runningTotal + purchaseTotal; // add the purchase total to the current payment method total to get updated total
+            newData = {
+                "total": newRunningTotal
+            }
+            updates = {};
+            updates['/inventory/paymentTotals/' + paymentMethod] = newData;
+            firebase.database().ref().update(updates); // update new payment total to firebase
+
+            // display payment confirmation success message to user
+            message = "You've successfully checked out an item!";
+            var notification = document.getElementById("notification");
+            notification.innerHTML = message;
+            notification.style.color = "#f89901";
+            
+            //close the modal, clear item list, purchase total, warning message, and uncheck radio buttons
+            var modal = document.getElementById('myModal');
+            modal.style.display = "none";
+            document.getElementById('modal-cart-items').innerHTML = '';
+            document.getElementById('modal-cart-total').innerHTML = '';
+            document.getElementById('warning').innerHTML = '';
+            document.getElementById('payment-method-1').checked = false;
+            document.getElementById('payment-method-2').checked = false;
+        });
+    } else { // if a payment method has not been chosen display warning to user
+        message = "You must choose a payment method!";
+        var warning = document.getElementById("warning");
+        warning.innerHTML = message;
+        warning.style.color = "red";
+    }
+
+}
