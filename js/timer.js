b3d50fb258e5d0a1611092f919aeb2bd30d38272
Zoe <min16009@byui.edu>
2018-03-28 15:47:29 -0600

    Fixing Fixes

--- js/timer.js
+++ js/timer.js
@@ -5,7 +5,6 @@ var seconds = 0,
     t;
 
 function showTime() {
-    console.log(seconds);
     breakP.innerHTML = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
     seconds++;
     if (seconds >= 60) {
@@ -16,9 +15,17 @@ function showTime() {
             hours++;
         }
     }
+    extra();
+}
+
+function extra() {
     localStorage.setItem('seconds', seconds);
     localStorage.setItem('minutes', minutes);
     localStorage.setItem('hours', hours);
+    if (minutes >= 15) {
+        document.getElementById('breakTimer').classList.remove('colorChange');
+        document.getElementById('breakTimer').classList.add('alertRed');
+    }
 }
 
 function timer() {
@@ -43,6 +50,7 @@ function clearTime() {
     minutes = 0;
     hours = 0;
     document.getElementById('breakTimer').style.display = 'none';
+    document.getElementById('breakTimer').classList.remove('alertRed');
     clearInterval(t);
     localStorage.removeItem('seconds');
     localStorage.removeItem('minutes');
