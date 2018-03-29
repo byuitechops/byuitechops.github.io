c8372a533fe9fdf8683e27628ae7630838a130f5
brookericson <eri11006@byui.edu>
2018-03-29 13:48:00 -0600

    Updated to display images from firebase storage

--- js/inventory.js
+++ js/inventory.js
@@ -28,9 +28,9 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
                             document.getElementById('adminlink').classList.remove('hide');
                         }
                     }
-                    else {
-                        window.location.replace("home.html");
-                    }
+                    //                    else {
+                    //                        window.location.replace("home.html");
+                    //                    }
                 }
             });
         });
@@ -47,12 +47,13 @@ function createTable() {
 
     var table = document.getElementById("inventory-table");
     table.innerHTML = ''; //clear any existing cells in the inventory table
-    var row = table.insertRow(-1);  //insert row containing table column headings
+    var row = table.insertRow(-1); //insert row containing table column headings
     row.insertCell(0).innerHTML = "<b>Item</b>";
-    var cell2 = row.insertCell(1).innerHTML = "<b>Price</b>";
-    var cell3 = row.insertCell(2).innerHTML = "<b>Stock</b>";
-    var cell4 = row.insertCell(3);
-    
+    var cell2 = row.insertCell(1).innerHTML = "<b>Image</b>";
+    var cell3 = row.insertCell(2).innerHTML = "<b>Price</b>";
+    var cell4 = row.insertCell(3).innerHTML = "<b>Stock</b>";
+    var cell5 = row.insertCell(4);
+
     var query = firebase.database().ref("inventory/items").orderByKey(); //get inventory items from firebase
     query.once("value")
         .then(function (snapshot) { //loop through each inventory item
@@ -61,6 +62,7 @@ function createTable() {
                 var childData = childSnapshot.val(); //gets data associated with item
                 var count = childData.count;
                 var price = parseFloat(childData.price).toFixed(2);
+                var image = childData.image;
 
                 //create new row with four cell into table
                 var row = table.insertRow(-1);
@@ -68,86 +70,118 @@ function createTable() {
                 var cell2 = row.insertCell(1);
                 var cell3 = row.insertCell(2);
                 var cell4 = row.insertCell(3);
+                var cell5 = row.insertCell(4);
 
                 //insert name, price, and quanity of item into cells
                 cell1.innerHTML = key;
-                cell2.innerHTML = price;
-                cell3.innerHTML = count;
-     
+                cell3.innerHTML = price;
+                cell4.innerHTML = count;
+
+                //insert image into cell2
+                if(image == ''){
+                    image = "default-image.png";
+                }
+                var div = document.createElement("div");
+                div.className = "image-container";
+                var storage = firebase.storage();
+                var storageRef = storage.ref();
+                var spaceRef = storageRef.child('images/' + image);
+                spaceRef.getDownloadURL().then(function (url) {
+                  div.style.backgroundImage = "url('"+url+"')";
+                }).catch(function (error) {
+                    console.log("There was an error retreiving " + image +" from firebase");
+                });        
+                 cell2.appendChild(div);
+
                 //give each cell an id
                 cell1.id = "item" + id;
-                cell2.id = "price" + id;
-                cell3.id = "count" + id;
-                
+                cell2.id = "image" + id;
+                cell3.id = "price" + id;
+                cell4.id = "count" + id;
+
                 //create the edit button by appending a span to cell 4
-                var updateButton = cell4.appendChild(document.createElement("span"));
+                var updateButton = cell5.appendChild(document.createElement("span"));
                 updateButton.innerHTML = "Edit";
                 updateButton.style.cursor = "pointer";
                 updateButton.style.color = "#0076c6";
-                
+
                 //add click eventlistener to span that calls displayUpdateModal function
                 updateButton.addEventListener("click", function () {
                     displayUpdateModal(key);
                 });
-              
+
                 //create the delete button by appending another span to cell 4
-                var deleteButton = cell4.appendChild(document.createElement("span"));
+                var deleteButton = cell5.appendChild(document.createElement("span"));
                 deleteButton.innerHTML = "&#x2715;";
                 deleteButton.style.cursor = "pointer";
-                
+
                 //add click eventlistener to span that calls delete function
                 deleteButton.addEventListener("click", function () {
                     deleteItem(key);
                 });
-                
+
                 id += 1; //increment id by 1
             });
-        
+
             //create a the last row in table to contain input fields for new inventory items
             var row = table.insertRow(-1);
             var cell1 = row.insertCell(0);
             var cell2 = row.insertCell(1);
             var cell3 = row.insertCell(2);
             var cell4 = row.insertCell(3);
-        
+            var cell5 = row.insertCell(4);
+
             //create three input elements
             var input1 = document.createElement("input");
             var input2 = document.createElement("input");
             var input3 = document.createElement("input");
+            var input4 = document.createElement("input");
         
             //give each input a new id
             input1.id = "input1";
             input2.id = "input2";
             input3.id = "input3";
+            input4.id = "input4";
         
             //set the type attribute on each input
             input1.setAttribute("type", "text");
-            input2.setAttribute("type", "number");
+            input2.setAttribute("type", "file");
             input3.setAttribute("type", "number");
+            input4.setAttribute("type", "number");
 
             //set the min range and step for input 2 and 3 to zero
-            input2.step = 0.01;
-            input2.min = 0;
+            input3.step = 0.01;
             input3.min = 0;
-        
+            input4.min = 0;
+
             //set the name for each input
             input1.name = "itemName";
-            input2.name = "price";
-            input3.name = "quantity";
+            input2.name = "image";
+            input3.name = "price";
+            input4.name = "quantity";
         
+            //apend label to cell2
+            var label = document.createElement("label");
+            label.innerHTML = "Choose a file";
+            label.htmlFor = "input2";
+            label.className = "custom-file-upload";
+            cell2.appendChild(label);
+
             //append each input to respective cell within new row
             cell1.appendChild(input1);
-            cell2.appendChild(input2);
+            label.appendChild(input2);
             cell3.appendChild(input3);
+            cell4.appendChild(input4);
         
+
             //append button to cell 4
-            var addButton = cell4.appendChild(document.createElement("button"));
+            var addButton = cell5.appendChild(document.createElement("button"));
             addButton.innerHTML = "Add";
             addButton.style.fontWeight = "bold";
-        
+
             //add click eventListener to call createItem function
             addButton.addEventListener("click", createItem);
-            
+
         });
 }
 
@@ -192,14 +226,14 @@ function displayUpdateModal(item) {
 
 //edits and updates selected item in firebase
 function editItem() {
-    
+
     // get item, name, price, and quantity
     var item = document.getElementById("name").innerHTML;
-    var price = parseFloat((document.getElementById("price").value)).toFixed(2); 
+    var price = parseFloat((document.getElementById("price").value)).toFixed(2);
     var quantity = document.getElementById("quantity").value;
-    
+
     //check that input fields are not empty, if not update new data for item to firebase
-    if (item != '' && price != '' && quantity != '') { 
+    if (item != '' && price != '' && quantity != '') {
         newData = {
             "count": quantity,
             "price": price
@@ -215,7 +249,7 @@ function editItem() {
 function deleteItem(item) {
     //display confirmation pop-up box prompting user to confirm the deletion
     var a = confirm("Are you sure you would like to delete" + item + "?");
-    
+
     //if the user confirms the deletion, delete the selected item from firebase
     if (a) {
         newData = {
@@ -231,12 +265,12 @@ function deleteItem(item) {
 
 //creates a new item and writes new item to firebase
 function createItem() {
-    
+
     //get the name, price, and quantity of new item from input values
     var item = document.getElementById("input1").value;
     var price = parseFloat((document.getElementById("input2").value)).toFixed(2);
     var quantity = document.getElementById("input3").value;
-    
+
     //check that each input has a value, if none of the inputs are empty, write the new item to firebase
     if (item != '' && price != '' && quantity != '') {
         newData = {
@@ -247,15 +281,11 @@ function createItem() {
         updates['/inventory/items/' + item] = newData;
         firebase.database().ref().update(updates);
         createTable();
-    }
-    else if (item == ''){ //if item input is empty display message to enter item name 
+    } else if (item == '') { //if item input is empty display message to enter item name 
         document.getElementById("warning").innerHTML = "Please enter an item name";
-    }
-    else if (price == ''){//if item input is empty display message to enter price 
+    } else if (price == '') { //if item input is empty display message to enter price 
         document.getElementById("warning").innerHTML = "Please enter a price for the new item";
-    }
-    else if (quantity == ''){//if item input is empty display message to enter quantity
+    } else if (quantity == '') { //if item input is empty display message to enter quantity
         document.getElementById("warning").innerHTML = "Please enter the number of items in stock";
     }
 }
-
