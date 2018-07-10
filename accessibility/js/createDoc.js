58c262117d57b916b098c8f168fb4df32fcf8227
Emma Fisher <emmarfisher99@gmail.com>
2018-07-10 12:46:24 -0600

    moving file

--- accessibility/js/createDoc.js
+++ accessibility/js/createDoc.js
@@ -44,12 +44,27 @@ function execute() {
         .then(function (response) {
             // Handle the results here (response.result has the parsed body).
             console.log('Response', response);
+            var newFileId = response.body.slice(33, 77);
+            move(newFileId);
 
         },
         function (err) {
             console.error('Execute error', err);
         });
 }
+
+function move(id) {
+    console.log('my id', id);
+    return gapi.client.drive.files.update({
+        'fileId': id,
+        'addParents': '0B3DpK7IUgwKBdmh6bUxPYWZsQjQ',
+        'removeParents': '0BztuMIt3a96YUUhjMWl3WElDUFk',
+        'supportsTeamDrives': 'true',
+        'resource': {}
+    });
+}
+
+
 gapi.load('client:auth2', function () {
     gapi.auth2.init({
         client_id: '275383619900-a03vtbvhm40mlne3dc1mkhq235k62eds.apps.googleusercontent.com'
