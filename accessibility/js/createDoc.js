22f489759887f0624b4f122cb1ef9136a08917b6
Miner <zminer@byui.edu>
2018-07-11 13:17:26 -0600

    test

--- accessibility/js/createDoc.js
+++ accessibility/js/createDoc.js
@@ -6,69 +6,68 @@ function authenticate() {
             scope: 'https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.appdata https://www.googleapis.com/auth/drive.apps.readonly https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.photos.readonly'
         })
         .then(function () {
-            console.log('Sign-in successful');
-        },
-        function (err) {
-            console.error('Error signing in', err);
-        });
+                console.log('Sign-in successful');
+            },
+            function (err) {
+                console.error('Error signing in', err);
+            });
 }
 
 function loadClient() {
     return gapi.client.load('https://content.googleapis.com/discovery/v1/apis/drive/v2/rest')
         .then(function () {
-            console.log('GAPI client loaded for API');
-        },
-        function (err) {
-            console.error('Error loading GAPI client for API', err);
-        });
+                console.log('GAPI client loaded for API');
+            },
+            function (err) {
+                console.error('Error loading GAPI client for API', err);
+            });
 }
 // Make sure the client is loaded and sign-in is complete before calling this method.
 function execute() {
     var title = document.getElementById('requestTitle').value;
     return gapi.client.drive.files.copy({
-        'fileId': '1B41vSP4ggurSr-FWFGfTXSmYTyD9cLUoBNYGCZ_t0v8',
-        'title': 'Transcript: ' + title,
-        'resource': {
-            'parents': [{
-                'id': '0B3DpK7IUgwKBMHRlOEZNQ0gwbHM'
-            }]
+            'fileId': '1B41vSP4ggurSr-FWFGfTXSmYTyD9cLUoBNYGCZ_t0v8',
+            'title': 'Transcript: ' + title,
+            'resource': {
+                "parents": [{
+                    "id": "0B3DpK7IUgwKBdmh6bUxPYWZsQjQ"
+                }]
+            },
+            'convert': 'false',
+            'ocr': 'false',
+            'supportsTeamDrives': 'true',
+            'visibility': 'DEFAULT',
 
-        },
-        'convert': 'false',
-        'ocr': 'false',
-        'supportsTeamDrives': 'true',
-        'visibility': 'DEFAULT',
-
-    })
+        })
 
         .then(function (response) {
-            // Handle the results here (response.result has the parsed body).
-            console.log('Response', response);
-            var newFileId = response.body.slice(33, 77);
-            move(newFileId);
+                // Handle the results here (response.result has the parsed body).
+                console.log('Response', response);
+                var newFileId = response.body.slice(33, 77);
+                // move(newFileId);
 
-        },
-        function (err) {
-            console.error('Execute error', err);
-        });
+            },
+            function (err) {
+                console.error('Execute error', err);
+            });
 }
 
 function move(id) {
     console.log('my id', id);
     return gapi.client.drive.files.update({
-        'fileId': id,
-        'addParents': '0B3DpK7IUgwKBdmh6bUxPYWZsQjQ',
-        'removeParents': '0BztuMIt3a96YUUhjMWl3WElDUFk',
-        'supportsTeamDrives': 'true',
-        'resource': {}
-    })
+            'fileId': id,
+            'addParents': '0B3DpK7IUgwKBdmh6bUxPYWZsQjQ',
+            'removeParents': '0BztuMIt3a96YUUhjMWl3WElDUFk',
+            'supportsTeamDrives': 'true',
+            'resource': {}
+        })
         .then(function (response) {
-            // Handle the results here (response.result has the parsed body).
-            console.log('Response', response);
-        },
-        function (err) {
-            console.error('Execute error', err);
-        });
+                // Handle the results here (response.result has the parsed body).
+                console.log('Response', response);
+            },
+            function (err) {
+                console.error('Execute error', err);
+            });
 }
 
 
