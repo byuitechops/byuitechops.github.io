6b383ed3e60da394d12c5e1182a50157a93e2220
Miner <zminer@byui.edu>
2018-07-17 20:01:18 -0600

    adding more stuff

--- accessibility/js/claim.js
+++ accessibility/js/claim.js
@@ -15,11 +15,55 @@ const settings = { /* your settings... */
 };
 db.settings(settings);
 
+// Check if Logged In
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        // User is signed in.
+    } else {
+        // No user is signed in.
+        window.location.assign('index.html');
+    }
+});
+
+var message = document.getElementById('message');
+
+function resetMessage() {
+    setTimeout(() => {
+        message.innerHTML = '';
+        message.style.color = 'black';
+    }, 10000);
+}
+
 // Get Data
 db.collection("accessibility").orderBy('priority').get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
         // console.log(`${doc.id} => ${doc.data().title}`);
-        var text = `<p>${doc.data().title},  ${doc.data().courseCode}, ${doc.data().priority}, ${doc.data().type}, ${doc.data().docURL}</p>`;
-        document.getElementById('text').insertAdjacentHTML('beforeend', text);
+        if (doc.data().transcriber == null || doc.data().transcriber == undefined) {
+            if (doc.data().type == "Transcript") {
+                var text = `<span>${doc.data().courseCode}</span><span>${doc.data().priority}</span><span>${doc.data().type}</span><span>${doc.data().title}</span><span>${doc.data().videoLength}</span><button onclick="claimItem('${doc.id}')">Claim</button>`;
+            } else {
+                var text = `<span>${doc.data().courseCode}</span><span>${doc.data().priority}</span><span>${doc.data().type}</span><span>${doc.data().title}</span><span></span><button onclick="claimItem('${doc.id}')">Claim</button>`;
+            }
+            document.getElementById('text').insertAdjacentHTML('beforeend', text);
+        }
     });
 });
+
+function claimItem(docId) {
+    var user = firebase.auth().currentUser;
+    db.collection('accessibility').doc(docId).update({
+            transcriber: user.displayName
+        })
+        .then(function () {
+            window.location.replace('home.html');
+            // message.innerHTML = 'Request has been made.';
+            // message.style.color = 'blue';
+            // resetMessage();
+        })
+        .catch(function (error) {
+            console.error('Error updating document: ', error);
+            message.innerHTML = 'There was an error making the request. Please try again.';
+            message.style.color = 'red';
+            resetMessage();
+        });
+}
--- accessibility/js/home.js
+++ accessibility/js/home.js
@@ -20,6 +20,7 @@ db.settings(settings);
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
         // User is signed in.
+        getTheData(user);
     } else {
         // No user is signed in.
         window.location.assign('index.html');
@@ -31,16 +32,27 @@ document.getElementById('btnLogout').addEventListener('click', function () {
     firebase.auth().signOut();
 });
 
-// Get Data
-db.collection("accessibility").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
-    });
-});
-
 function resetMessage() {
     setTimeout(() => {
         message.innerHTML = "";
         message.style.color = "black";
     }, 10000);
+}
+
+function getTheData(user) {
+    // Get Data
+    var data = db.collection("accessibility").where("transcriber", "==", user.displayName).orderBy('priority').get()
+        .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                // console.log(`${doc.id} => ${doc.data().type}`);
+                if (doc.data().transcriber != null || doc.data().transcriber != undefined) {
+                    if (doc.data().type == "Transcript") {
+                        var text = `<span>${doc.data().courseCode}</span><span>${doc.data().priority}</span><span>${doc.data().type}</span><span>${doc.data().title}</span><span>${doc.data().videoLength}</span><button onclick="claimItem('${doc.id}')">Claim</button>`;
+                    } else {
+                        var text = `<span>${doc.data().courseCode}</span><span>${doc.data().priority}</span><span>${doc.data().type}</span><span>${doc.data().title}</span><span></span><button onclick="claimItem('${doc.id}')">Claim</button>`;
+                    }
+                    document.getElementById('text').insertAdjacentHTML('beforeend', text);
+                }
+            });
+        });
 }
