5c04dddc50de3bb3ee20b473513de7e74bd8f31e
Miner <zminer@byui.edu>
2018-08-17 15:52:23 -0600

    update

--- accessibility/js/place.js
+++ accessibility/js/place.js
@@ -108,7 +108,7 @@ function displayEmbedCode(link, height, seconds, title) {
         var id = link.slice(link.indexOf("watch?v=") + 8, (link.indexOf("watch?v=") + 9) + 11);
         // console.log(id);
         var html = `<p><iframe width="560" height="${height}px" src="https://www.youtube-nocookie.com/embed/${id}?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe><br>
-                    (${time} mins, <a href="#">${title} Transcript</a>)</p>`;
+                    (${time} mins, <a href="#" target="_blank">${title} Transcript</a>)</p>`;
         document.getElementById('intro').innerText = html;
 
         var html = `<div id="buttons"><button id="placeButton" onclick="cancel()">Close</button></div>`;
@@ -117,7 +117,7 @@ function displayEmbedCode(link, height, seconds, title) {
         var id = link.slice(link.indexOf(".be/") + 4, (link.indexOf(".be/") + 4) + 11);
         // console.log(id);
         var html = `<p><iframe width="560" height="${height}px" src="https://www.youtube-nocookie.com/embed/${id}?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe><br>
-                    (${time} mins, <a href="#">${title} Transcript</a>)</p>`;
+                    (${time} mins, <a href="#" target="_blank">${title} Transcript</a>)</p>`;
         document.getElementById('intro').innerText = html;
 
         var html = `<div id="buttons"><button id="placeButton" onclick="cancel()">Close</button></div>`;
@@ -127,7 +127,7 @@ function displayEmbedCode(link, height, seconds, title) {
         // console.log(id);
         var html = `<p><iframe id="kaltura_player_1532969286" src="http://cdnapi.kaltura.com/p/1157612/sp/115761200/embedIframeJs/uiconf_id/33020032/partner_id/1157612?iframeembed=true&playerId=kaltura_player_1532969286&entry_id=${id}&flashvars[streamerType]=auto"
                      width="560" height="${height}" allowfullscreen webkitallowfullscreen mozAllowFullScreen allow="autoplay; fullscreen; encrypted-media" frameborder="0"></iframe><br>
-                     (${time} mins, <a href="#">${title} Transcript</a>)</p>`;
+                     (${time} mins, <a href="#" target="_blank">${title} Transcript</a>)</p>`;
         document.getElementById('intro').innerText = html;
 
         var html = `<div id="buttons"><button id="placeButton" onclick="cancel()">Close</button></div>`;
@@ -136,15 +136,27 @@ function displayEmbedCode(link, height, seconds, title) {
         var id = link.slice(link.indexOf("vimeo.com/") + 10, (link.indexOf("vimeo.com/") + 10) + 9);
         // console.log(id);
         var html = `<p><iframe src="https://player.vimeo.com/video/${id}?title=0&byline=0&portrait=0" width="560" height="${height}px" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe><br>
-        (${time} mins, <a href="#">${title} Transcript</a>)</p>`;
+        (${time} mins, <a href="#" target="_blank">${title} Transcript</a>)</p>`;
+        document.getElementById('intro').innerText = html;
+
+        var html = `<div id="buttons"><button id="placeButton" onclick="cancel()">Close</button></div>`;
+        document.getElementById('modal-content').insertAdjacentHTML('beforeend', html);
+    } else if (link.includes("fod.infobase.com")) {
+        var id = link.slice(link.indexOf("loid=") + 5, (link.indexOf("loid=") + 5) + 5);
+        // console.log(id);
+        var html = `<p><iframe allow='encrypted-media' height='${height}' frameborder='0' width='560' style='border: 1px solid #ddd;'  src='https://byui.idm.oclc.org/login?url=https://fod-infobase-com.byui.idm.oclc.org/OnDemandEmbed.aspx?token=42704&wID=104034&loid=${id}&plt=FOD&w=560&h=360' allowfullscreen >&nbsp;</iframe><br>
+                     (${time} mins, <a href="#" target="_blank">${title} Transcript</a>)</p>`;
         document.getElementById('intro').innerText = html;
 
         var html = `<div id="buttons"><button id="placeButton" onclick="cancel()">Close</button></div>`;
         document.getElementById('modal-content').insertAdjacentHTML('beforeend', html);
     } else {
-        var html = `<p><iframe width="560" height="315" src="${link}" frameborder="0" allowfullscreen></iframe><br> 
-        (${time} mins, <a href="#">${title} Transcript</a>)</p>`;
-        document.getElementById('intro').innerText = html;
+        document.getElementById('intro').classList.remove('intro');
+        document.getElementById('intro').style.fontSize = '1.25em';
+        var html = `<p><a href='${link}' target="_blank">Go to this link and get the embed code to place</a><br>
+        Copy the rest of this and place it in with the embed in a single p tag<br>
+        (${time} mins, <a href="#" target="_blank">${title} Transcript</a>)</p>`;
+        document.getElementById('intro').innerHTML = html;
 
         var html = `<div id="buttons"><button id="placeButton" onclick="cancel()">Close</button></div>`;
         document.getElementById('modal-content').insertAdjacentHTML('beforeend', html);
@@ -161,7 +173,7 @@ function displayLinkCode(link, seconds, title) {
         var id = link.slice(link.indexOf("watch?v=") + 8, (link.indexOf("watch?v=") + 9) + 11);
         // console.log(id);
         var html = `<p><a href="https://www.youtube-nocookie.com/embed/${id}?rel=0&amp;showinfo=0" target="_blank">${title}</a>
-                    (${time} mins, <a href="#">${title} Transcript</a></p>`;
+                    (${time} mins, <a href="#" target="_blank">${title} Transcript</a></p>`;
         document.getElementById('intro').innerText = html;
 
         var html = `<div id="buttons"><button id="placeButton" onclick="cancel()">Close</button></div>`;
@@ -170,7 +182,7 @@ function displayLinkCode(link, seconds, title) {
         var id = link.slice(link.indexOf(".be/") + 4, (link.indexOf(".be/") + 4) + 11);
         // console.log(id);
         var html = `<p><a href="https://www.youtube-nocookie.com/embed/${id}?rel=0&amp;showinfo=0 target="_blank">${title}</a>
-        (${time} mins, <a href="#">${title} Transcript</a>)</p>`;
+        (${time} mins, <a href="#" target="_blank">${title} Transcript</a>)</p>`;
         document.getElementById('intro').innerText = html;
 
         var html = `<div id="buttons"><button id="placeButton" onclick="cancel()">Close</button></div>`;
@@ -179,7 +191,7 @@ function displayLinkCode(link, seconds, title) {
         var id = link.slice(link.indexOf("/0_") + 1, (link.indexOf("/0_") + 1) + 10);
         // console.log(id);
         var html = `<p><a href="http://cdnapi.kaltura.com/p/1157612/sp/115761200/embedIframeJs/uiconf_id/33020032/partner_id/1157612?iframeembed=true&playerId=kaltura_player_1532969286&entry_id=${id}&flashvars[streamerType]=auto target="_blank">${title}</a>
-        (${time} mins, <a href="#">${title} Transcript</a>)</p>`;
+        (${time} mins, <a href="#" target="_blank">${title} Transcript</a>)</p>`;
         document.getElementById('intro').innerText = html;
 
         var html = `<div id="buttons"><button id="placeButton" onclick="cancel()">Close</button></div>`;
@@ -188,14 +200,23 @@ function displayLinkCode(link, seconds, title) {
         var id = link.slice(link.indexOf("vimeo.com/") + 10, (link.indexOf("vimeo.com/") + 10) + 9);
         // console.log(id);
         var html = `<p><a href="https://player.vimeo.com/video/${id}?title=0&byline=0&portrait=0 target="_blank">${title}</a>
-        (${time} mins, <a href="#">${title} Transcript</a>)</p>`;
+        (${time} mins, <a href="#" target="_blank">${title} Transcript</a>)</p>`;
+        document.getElementById('intro').innerText = html;
+
+        var html = `<div id="buttons"><button id="placeButton" onclick="cancel()">Close</button></div>`;
+        document.getElementById('modal-content').insertAdjacentHTML('beforeend', html);
+    } else if (link.includes("fod.infobase.com")) {
+        var id = link.slice(link.indexOf("loid=") + 5, (link.indexOf("loid=") + 5) + 5);
+        // console.log(id);
+        var html = `<p><a href='https://byui.idm.oclc.org/login?url=https://fod-infobase-com.byui.idm.oclc.org/OnDemandEmbed.aspx?token=42704&wID=104034&loid=${id}&plt=FOD&w=560&h=360' target="_blank">${title}</a>
+                     (${time} mins, <a href="#" target="_blank">${title} Transcript</a>)</p>`;
         document.getElementById('intro').innerText = html;
 
         var html = `<div id="buttons"><button id="placeButton" onclick="cancel()">Close</button></div>`;
         document.getElementById('modal-content').insertAdjacentHTML('beforeend', html);
     } else {
-        var html = `<p><a href='${link}'>${title}</a>
-        (${time} mins, <a href="#">${title} Transcript</a>)</p>`;
+        var html = `<p><a href='${link}' target="_blank">${title}</a>
+        (${time} mins, <a href="#" target="_blank">${title} Transcript</a>)</p>`;
         document.getElementById('intro').innerText = html;
 
         var html = `<div id="buttons"><button id="placeButton" onclick="cancel()">Close</button></div>`;
