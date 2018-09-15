35199f6cfa3172b440f22db27cb8390380053f68
Miner <zminer@byui.edu>
2018-09-15 10:01:07 -0600

    new schedules

--- js/schedule.js
+++ js/schedule.js
@@ -37,10 +37,10 @@
                             document.getElementById('scheduleFrame').classList.remove('hide');
                             if (shot[titles] == 'canvas 1') {
                                 // If canvas 1 set iframe to show canvas 1 schedule
-                                document.getElementById('scheduleFrame').setAttribute('src', 'https://docs.google.com/spreadsheets/d/1PxiGqAp7b8HwwwDNvHUQiLs961PAAcLChBmYfYywPO4/edit?usp=sharing');
+                                document.getElementById('scheduleFrame').setAttribute('src', 'https://docs.google.com/spreadsheets/d/1P8rKKlO4BxLonT840yB7SeO59bjnmLnjvYNiurNaywg/edit?usp=sharing');
                             } else if (shot[titles] == 'canvas 2') {
                                 // If canvas 2 set iframe to show canvas 2 schedule
-                                document.getElementById('scheduleFrame').setAttribute('src', 'https://docs.google.com/spreadsheets/d/1rExB8ZbCJ8saPrdu1HvIMLJuPq7KygITzdNGrWctYe4/edit?usp=sharing');
+                                document.getElementById('scheduleFrame').setAttribute('src', 'https://docs.google.com/spreadsheets/d/1TaUkWSifGGYkE5um2Rjtfi_Q37EzPUWmY-_X87sBQSo/edit?usp=sharing');
                             }
                         }
 
@@ -61,9 +61,9 @@ function showTeam(team) {
     var frame = document.getElementById('scheduleFrame');
     if (team == 'canvas 1') {
         // If canvas 1 set iframe to show canvas 1 schedule
-        frame.setAttribute('src', 'https://docs.google.com/spreadsheets/d/1PxiGqAp7b8HwwwDNvHUQiLs961PAAcLChBmYfYywPO4/edit?usp=sharing');
+        frame.setAttribute('src', 'https://docs.google.com/spreadsheets/d/1P8rKKlO4BxLonT840yB7SeO59bjnmLnjvYNiurNaywg/edit?usp=sharing');
     } else if (team == 'canvas 2') {
         // If canvas 2 set iframe to show canvas 2 schedule
-        frame.setAttribute('src', 'https://docs.google.com/spreadsheets/d/1rExB8ZbCJ8saPrdu1HvIMLJuPq7KygITzdNGrWctYe4/edit?usp=sharing');
+        frame.setAttribute('src', 'https://docs.google.com/spreadsheets/d/1TaUkWSifGGYkE5um2Rjtfi_Q37EzPUWmY-_X87sBQSo/edit?usp=sharing');
     }
 }
