d40a48c2abf54e666f91f53d11088b898a045e1f
Miner <zminer@byui.edu>
2018-09-17 09:46:34 -0600

    update

--- accessibility/js/master.js
+++ accessibility/js/master.js
@@ -84,8 +84,8 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
     modal.style.display = "none";
-    // document.getElementById('docData').innerHTML = "";  
-    // document.getElementById('deleteButton').innerHTML = "";
+    document.getElementById('docData').innerHTML = "";  
+    document.getElementById('deleteButton').innerHTML = "";
 }
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
