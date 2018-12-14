c96870018a4f909a45c0135460ddcf372cc64149
Lucas Wargha <lwargha@byui.edu>
2018-12-14 08:59:06 -0700

    Grad Banner

--- portal/js/events.js
+++ portal/js/events.js
@@ -1,2 +1,25 @@
-//  reads from firestore and searches for a matching date.
- db.collection("users").doc().where("birthday", )
+// //  reads from firestore and searches for a matching date.
+// var today = new Date().toString();
+// var editToday = today.slice(4, 10);
+
+// db.collection("users").where("info.birthday", "==", editToday).get()
+//     .then(function (querySnapshot) {
+//         console.log(querySnapshot);
+//     })
+
+// //handles the modal box for the event
+// var modal = document.getElementById('myModal');
+// modal.style.display = "block";
+// var span = document.getElementsByClassName("close")[0];
+// span.onclick = function () {
+//     modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function (event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
+
+// modal.style.display = "block";
