1394fc7fcef61cfadb41b053fec061324046457d
lwargha <43412878+lwargha@users.noreply.github.com>
2019-01-29 15:36:15 -0700

    New Accessibility Script working

--- newAccessibility/js/main.js
+++ newAccessibility/js/main.js
@@ -5,12 +5,12 @@
 //****************************************************
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyBWv05RlAPUpAts6LNXgG5-wsdhd9jXafg",
-    authDomain: "byui-accessability.firebaseapp.com",
-    databaseURL: "https://byui-accessability.firebaseio.com",
-    projectId: "byui-accessability",
-    storageBucket: "byui-accessability.appspot.com",
-    messagingSenderId: "275383619900"
+    apiKey: "AIzaSyAIcGQ94aGJRMZihtoTcmMK7j3NavnPEOs",
+    authDomain: "byui-accessibility-redemption.firebaseapp.com",
+    databaseURL: "https://byui-accessibility-redemption.firebaseio.com",
+    projectId: "byui-accessibility-redemption",
+    storageBucket: "byui-accessibility-redemption.appspot.com",
+    messagingSenderId: "630332651011"
 };
 firebase.initializeApp(config);
 // Initialize Cloud Firestore through Firebase
@@ -38,7 +38,9 @@ firebase.auth().onAuthStateChanged(function (user) {
         } else {
             if (window.location.pathname != '/home.html') {
                 window.location.assign('home.html');
-            } else {}
+            } else {
+                
+            }
         }
     } else {
         // window.location.assign('home.html');
@@ -49,7 +51,7 @@ firebase.auth().onAuthStateChanged(function (user) {
 // Logout of firebase and website
 function userLogout() {
     console.log("It's working");
-        // firebase.auth().signOut();
+    // firebase.auth().signOut();
 }
 
 // Initialize Firebase
