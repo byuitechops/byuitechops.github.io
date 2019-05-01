226d16b20d9292f165fb5a8f275701d5af92d5c4
Smoot <cstroop@byui.edu>
2019-05-01 09:47:07 -0600

    portal3

---
+++ portal3/js/main.js
@@ -0,0 +1,33 @@
+var slideIndex = 0;
+var slides = document.getElementsByClassName("mySlides");
+
+showSlides();
+
+function showSlides() {
+  var i;
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 8000); // Change image every 10 seconds
+}
+
+function currentSlide(no) {
+  var i;
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex = no;
+  slides[no - 1].style.display = "block";
+}
+
+function plusSlides(n) {
+  var newslideIndex = slideIndex + n;
+  if (newslideIndex < 4 && newslideIndex > 0) {
+    currentSlide(newslideIndex);
+  }
+}
