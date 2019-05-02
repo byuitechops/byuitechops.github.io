d5a80661c26fa45844585e5a3b7ed45abaf429da
Hillman <srh235@byui.edu>
2019-05-02 14:12:06 -0600

    portal 3.0 updates

--- portal3/js/main.js
+++ portal3/js/main.js
@@ -1,11 +1,17 @@
+/**********************************************
+ * MAIN.JS | Quality Assurance Javascript File
+ * 
+ * 
+ *********************************************/
+
 var slideIndex = 0;
 var slides = document.getElementsByClassName("mySlides");
 
 showSlides();
 
 function showSlides() {
-  var i;
-  for (i = 0; i < slides.length; i++) {
+
+  for (var i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slideIndex++;
@@ -17,8 +23,8 @@ function showSlides() {
 }
 
 function currentSlide(no) {
-  var i;
-  for (i = 0; i < slides.length; i++) {
+
+  for (var i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slideIndex = no;
