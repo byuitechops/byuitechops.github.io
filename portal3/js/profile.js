212824ccb7bd90027bd3620f04338fddd4a96ab0
Smoot <cstroop@byui.edu>
2019-05-10 15:24:08 -0600

    mostly functional profile page

--- portal3/js/profile.js
+++ portal3/js/profile.js
@@ -1,9 +1,13 @@
 /***********************************************************
-* Profile.js Table of Conttents
-* A. Team Points
-********************************************************** */
+ * Profile.js Table of Conttents
+ * A. Team Points
+ * B. Redeem Hours
+ * C. Profile
+ * D. Sign Out
+ * E. Page Loader
+ ********************************************************** */
 const score = db.collection("team").doc('points');
-const toolTip = document.getElementById("clock");
+const redeemClock = document.getElementById("clock");
 const toolTipBox = document.getElementById("toolTipBox");
 const submitChanges = document.getElementById("submitInfoChanges");
 const redeemConfirmBtn = document.getElementById("redeembtn");
@@ -16,14 +20,18 @@ const cancelBtn = document.getElementById("cancel");
 const hoursRedeemed = document.getElementById("resultRedeem");
 const desiredTime = document.getElementById("timeDesired");
 const confirmRedeem = document.getElementById("confirmRedeem");
+const info = document.getElementById('info');
+const aboutMe = document.getElementById('aboutMe');
+const img = document.getElementById('arrowImg');
+const cancelChanges = document.getElementById("cancelInfoChanges");
 var results = []; //will be used as part of the results for 
 var birthdayPopulate;
 var sameBirthday = true;
 var doneUser = false;
 var user = firebase.auth().currentUser;
 /***********************************************************
-* A. Team Points
-************************************************************/
+ * A. Team Points
+ ************************************************************/
 // A function to update the points. This is currently set up that the teams are working together
 // There is a const written to shorten the code and make it more readable.
 function updateTeamPoints(pointsToAdd, activityType, timeStamp) {
@@ -78,9 +86,9 @@ function submitTeamPoints() {
         points = 5;
     } else if (activityType == 'Leading a PD event') {
         points = 1;
-    } else if (activityType == 'Going to the FTC'){
+    } else if (activityType == 'Going to the FTC') {
         points = 5;
-    } else if (activityType == "Filling up one water bottle"){
+    } else if (activityType == "Filling up one water bottle") {
         points = 2;
     }
 
@@ -90,36 +98,33 @@ function showResults() {
     console.log("Total = " + results[0]);
 }
 /***********************************************************
-* B. Redeem Hours
-************************************************************/
-toolTip.addEventListener("click", () => {
-    if (toolTipBox.style.visibility == "hidden") {
-        toolTipBox.style.visibility = "visible";
+ * B. Redeem Hours
+ ************************************************************/
+redeemClock.addEventListener("click", () => {
+    if (toolTipBox.classList == "hide") {
+        toolTipBox.classList.remove("hide");
     } else {
-        toolTipBox.style.visibility = "hidden";
+        toolTipBox.classList.add("hide");
     }
 })
-
 //showing up the redeem tool
 redeemBtn.addEventListener("click", () => {
-    if (redeemTool.style.visibility == "hidden") {
-        redeemTool.style.visibility = "visible";
-        toolTipBox.style.visibility = "hidden";
+    if (redeemTool.classList == "hide") {
+        redeemTool.classList.remove("hide");
+        toolTipBox.classList.add("hide");
     } else {
-        redeemTool.style.visibility = "hidden";
+        redeemTool.classList.add("hide");
     }
 })
-
 //when the user presses the "cancel" button, goes back to the screen
 cancelBtn.addEventListener('click', () => {
-    redeemTool.style.visibility = "hidden";
+    redeemTool.classList.add("hide");
 })
-
 // when the user clicks the "redeem" button, it shows the confirm
 redeemConfirmBtn.addEventListener('click', () => {
-    redeemTool.style.visibility = "hidden";
+    redeemTool.classList.remove("hide");
     if (data.time.accumulatedTime < desiredTime.value) {
-        redeemTool.style.visibility = "hidden";
+        redeemTool.classList.remove("hide");
         alert("You don't have enough accumulated hours to redeem the time desired");
     } else {
         db.collection("users").doc(userId).update({
@@ -133,61 +138,28 @@ redeemConfirmBtn.addEventListener('click', () => {
                 // An error happened.
             });
         if (data.time.accumulatedTime < 10) {
-            hoursRedeemed.innerText = "0" + desiredTime.value + ":00";
+            hoursRedeemed.innerHTML = "0" + desiredTime.value + ":00";
         } else {
-            hoursRedeemed.innerText = desiredTime.value + ":00";
+            hoursRedeemed.innerHTML = desiredTime.value + ":00";
         }
-        confirmRedeem.style.visibility = "visible";
+        confirmRedeem.classList.remove("hide");
     }
 })
-
 // When the use clicks ok close confirm
 okBtn.addEventListener('click', () => {
-    confirmRedeem.style.visibility = "hidden";
+    confirmRedeem.classList.add("hide");
 })
-
-
+/***********************************************************
+ * C. Profile
+ ************************************************************/
 //allows the user to edit his/her information
 editBtn.addEventListener("click", () => {
-    editDiv.style.visibility = "visible";
-    document.getElementById("displayBirthday").style.visibility = "visible";
-    document.getElementById("editBirthdayMonth").style.visibility = "hidden";
-    document.getElementById("editBirthdayDay").style.visibility = "hidden";
+    editDiv.classList.remove("hide");
+    document.getElementById("displayBirthday").classList.remove("hide");
+    document.getElementById("editBirthdayMonth").classList.add("hide");
+    document.getElementById("editBirthdayDay").classList.add("hide");
     populateInfoEdit();
 })
-
-/***********************************************************
-* C. Sign Out
-************************************************************/
-//handles the sign out button
-document.getElementById("signOutBtn").addEventListener("click", () => {
-    firebase.auth().signOut().then(function () {
-        // Sign-out successful.
-    }).catch(function (error) {
-        // An error happened.
-    });
-})
-
-// dropdown toggleView funcionality 
-
-function toggleView() {
-    var info = document.getElementById('info');
-    var aboutMe = document.getElementById('aboutMe');
-    var img = document.getElementById('arrowImg');
-    // If info is shown
-    if (info.style.height == "20%") {
-        info.style.height = "0%";
-        aboutMe.style.height = "60%";
-        aboutMe.style.overflow = "auto";
-        img.style.transform = "rotate(0deg)";
-    } else {
-        info.style.height = "20%";
-        aboutMe.style.height = "40%";
-        aboutMe.style.overflow = "hidden";
-        img.style.transform = "rotate(180deg)";
-    }
-}
-
 //sends to firebase info changes made by the user
 // Initialize Firebase
 submitChanges.addEventListener("click", () => {
@@ -214,20 +186,16 @@ submitChanges.addEventListener("click", () => {
             }).catch(function (error) {
                 // An error happened.
             });
-        editDiv.style.visibility = "hidden";
+        editDiv.classList.add("hide");
     }
 })
-
-
-var cancelChanges = document.getElementById("cancelInfoChanges");
 cancelChanges.addEventListener("click", () => {
-    editDiv.style.visibility = "hidden";
-    document.getElementById('editTrack').style.visibility = "hidden";
-    document.getElementById("displayBirthday").style.visibility = "hidden";
-    document.getElementById("editBirthdayMonth").style.visibility = "hidden";
-    document.getElementById("editBirthdayDay").style.visibility = "hidden";
+    editDiv.classList.add("hide");
+    document.getElementById('editTrack').classList.add("hide");
+    document.getElementById("displayBirthday").classList.add("hide");
+    document.getElementById("editBirthdayMonth").classList.add("hide");
+    document.getElementById("editBirthdayDay").classList.add("hide");
 })
-
 //populates de input boxes with what we already have from the user
 function populateInfoEdit() {
     db.collection("users").doc(userId).get()
@@ -238,7 +206,7 @@ function populateInfoEdit() {
             document.getElementById("editPhone").setAttribute("value", `${doc.data().info.phoneNumber}`);
             document.getElementById("editMajor").setAttribute("value", `${doc.data().info.major}`);
             //document.getElementById(idTrack).setAttribute("selected", "selected");
-            document.getElementById("editTrack").style.visibility = "visible";
+            document.getElementById("editTrack").classList.remove("hide");
             if (idTrack == 'Winter/Spring') {
                 document.getElementById("editTrack").selectedIndex = 0;
             } else if (idTrack == 'Spring/Fall') {
@@ -253,59 +221,69 @@ function populateInfoEdit() {
 
             // document.getElementById("editTrack").setAttribute("value", `${doc.data().info.track}`);
             document.getElementById("editGradDate").setAttribute("value", `${doc.data().info.graduation}`);
-            document.getElementById("editAboutMe").innerText = doc.data().info.aboutMe;
+            document.getElementById("editAboutMe").innerHTML = doc.data().info.aboutMe;
             document.getElementById("displayBirthday").setAttribute("value", `${doc.data().info.birthday}`);
         })
 }
-
 //handles the event listener for the birthday call
 //if the birthday doesn't get changed, by the following event listener, then it will keep the same on firebase
 document.getElementById("displayBirthday").addEventListener("click", () => {
-    document.getElementById("displayBirthday").style.visibility = "hidden";
-    document.getElementById("editBirthdayMonth").style.visibility = "visible";
-    document.getElementById("editBirthdayDay").style.visibility = "visible";
+    document.getElementById("displayBirthday").classList.add("hide");
+    document.getElementById("editBirthdayMonth").classList.remove("hide");
+    document.getElementById("editBirthdayDay").classList.remove("hide");
     sameBirthday = false;
 })
-//Loads the page with all user's information
+/***********************************************************
+ * D. Sign Out
+ ************************************************************/
+//handles the sign out button
+document.getElementById("signOutBtn").addEventListener("click", () => {
+    firebase.auth().signOut().then(function () {
+        // Sign-out successful.
+    }).catch(function (error) {
+        // An error happened.
+    });
+})
+/***********************************************************
+ * E. Page Loader
+ ************************************************************/
 function loadPage() {
+    db.collection("users").where("name", "==", userName)
+        .onSnapshot((querySnapshot) => {
+            data = querySnapshot.docs[0].data();
+            userId = querySnapshot.docs[0].id;
+            preferance = data.viewMode;
+            db.collection("users").doc(userId)
+                .onSnapshot((querrySnapshot) => {
+                    console.log(querrySnapshot.data());
+                    const myData = querrySnapshot.data();
+                    document.getElementById("dbName").innerHTML = myData.nameDisplay;
+                    document.getElementById("dbTitle").innerHTML = "Title: " + myData.title;
+                    document.getElementById("dbTeam").innerHTML = "Team: " + myData.team;
+                    document.getElementById("dbPhone").innerHTML = "T: " + myData.info.phoneNumber;
+                    document.getElementById("dbEmail").innerHTML = "E-mail: " + myData.info.email;
+                    document.getElementById("dbMajor").innerHTML = "Major: " + myData.info.major;
+                    document.getElementById("dbTrack").innerHTML = "Track: " + myData.info.track;
+                    document.getElementById("dbGradDate").innerHTML = "Graduation Date: " + myData.info.graduation;
+                    //document.getElementById("dbTyping").innerHTML = "Typing Speed: " + myData.info.speed;
+                    document.getElementById("dbAboutMe").innerHTML = myData.info.aboutMe;
+                    document.getElementById("dbBirth").innerHTML = `Birthday: ` + myData.info.birthday;
+                    birthdayPopulate = myData.info.birthday;
+                    //fills in accumulated time
+                    if (data.time.accumulatedTime < 10) {
+                        document.getElementById("accumulated").innerHTML = "0" + myData.time.accumulatedTime + ":00";
+                        document.getElementById("redeemTime").innerHTML = "0" + myData.time.accumulatedTime + ":00";
+                    } else {
+                        document.getElementById("accumulated").innerHTML = myData.time.accumulatedTime + ":00";
+                        document.getElementById("redeemTime").innerHTML = myData.time.accumulatedTime + ":00";
+                    }
+                    //displays lead/admin tools only for the right people
+                    if (myData.admin || myData.title == "Project Lead") {}
+                    if (myData.storeManager == true) {}
+                })
+        })
     db.collection("team").doc('points').get()
         .then(function (doc) {
             results.push(doc.data().points);
-        })
-    var photo = data.info.photo;
-    firebase.storage().ref().child(`profile/${photo}`).getDownloadURL().then(function (url) {
-        document.getElementById(`profilePic`).setAttribute('src', url);
-        //return;
-    }).catch(function (error) {
-        return error;
-    });
-    db.collection("users").doc(userId).get()
-        .then(function (doc) {
-            const myData = doc.data();
-            document.getElementById("dbName").innerText = myData.nameDisplay;
-            document.getElementById("dbTitle").innerText = "Title: " + myData.title;
-            document.getElementById("dbTeam").innerText = "Team: " + myData.team;
-            document.getElementById("dbPhone").innerText = "T: " + myData.info.phoneNumber;
-            document.getElementById("dbEmail").innerText = "E-mail: " + myData.info.email;
-            document.getElementById("dbMajor").innerText = "Major: " + myData.info.major;
-            document.getElementById("dbTrack").innerText = "Track: " + myData.info.track;
-            document.getElementById("dbGradDate").innerText = "Graduation Date: " + myData.info.graduation;
-            //document.getElementById("dbTyping").innerText = "Typing Speed: " + myData.info.speed;
-            document.getElementById("dbAboutMe").innerText = myData.info.aboutMe;
-            document.getElementById("dbBirth").innerText = `Birthday: ` + myData.info.birthday;
-            birthdayPopulate = myData.info.birthday;
-            //fills in accumulated time
-            if (data.time.accumulatedTime < 10) {
-                document.getElementById("accumulated").innerText = "0" + myData.time.accumulatedTime + ":00";
-                document.getElementById("redeemTime").innerText = "0" + myData.time.accumulatedTime + ":00";
-            } else {
-                document.getElementById("accumulated").innerText = myData.time.accumulatedTime + ":00";
-                document.getElementById("redeemTime").innerText = myData.time.accumulatedTime + ":00";
-            }            
-            //displays lead/admin tools only for the right people
-            if (myData.admin || myData.title == "Project Lead") {
-            }
-            if (myData.storeManager == true) {
-            }
         })
 }
--- portal3/js/schedule.js
+++ portal3/js/schedule.js
@@ -6,39 +6,42 @@ ssCanvas1 = "https://docs.google.com/spreadsheets/d/1g53WojmX1wG7fTGU1Q1Hg93-QJ5
 
 
 document.getElementById("selectTeam").addEventListener('change', () => {
-    db.collection("users").doc(userId).get()
-    .then(function (doc) {
-        const myData = doc.data();
-        var team = document.getElementById("teams");
-        var iframe = document.getElementById("spreadsheet");
-        //checks for permissions to see both teams' schedule
-        if (myData.admin || myData.lead) {
-            document.getElementById("admin").style.visibility = "visible";
-            team.style.visibility = "visible";
-            //clicked on team 1
-            document.getElementById("selectTeam").addEventListener("change", () => {
-                if (document.getElementById("selectTeam").value == "1") {
-                    iframe.src = ssCanvas1
-                    document.getElementById("newTab").href = ssCanvas1;
-                } else {
-                    iframe.src = ssCanvas2
-                    document.getElementById("newTab").href = ssCanvas2;
-                }
-            })
-        } else {
-            if (myData.team == "lms") {
-                team.style.visibility = "visible";
-                iframe.src = ssCanvas1;
-                document.getElementById("newTab").href = ssCanvas1;
-            } else if (myData.team == "accessibility") {
-                team.style.visibility = "visible";
-                iframe.src = ssCanvas2;
-                document.getElementById("newTab").href = ssCanvas2;
+    db.collection("users").where("name", "==", userName)
+        .onSnapshot((querySnapshot) => {
+            data = querySnapshot.docs[0].data();
+            userId = querySnapshot.docs[0].id;
+            preferance = data.viewMode;
+            db.collection("users").doc(userId).get()
+                .then(function (doc) {
+                    const myData = doc.data();
+                    var team = document.getElementById("teams");
+                    var iframe = document.getElementById("spreadsheet");
+                    //checks for permissions to see both teams' schedule
+                    if (myData.admin || myData.lead) {
+                        document.getElementById("admin").style.visibility = "visible";
+                        team.style.visibility = "visible";
+                        //clicked on team 1
+                        document.getElementById("selectTeam").addEventListener("change", () => {
+                            if (document.getElementById("selectTeam").value == "1") {
+                                iframe.src = ssCanvas1
+                                document.getElementById("newTab").href = ssCanvas1;
+                            } else {
+                                iframe.src = ssCanvas2
+                                document.getElementById("newTab").href = ssCanvas2;
+                            }
+                        })
+                    } else {
+                        if (myData.team == "lms") {
+                            team.style.visibility = "visible";
+                            iframe.src = ssCanvas1;
+                            document.getElementById("newTab").href = ssCanvas1;
+                        } else if (myData.team == "accessibility") {
+                            team.style.visibility = "visible";
+                            iframe.src = ssCanvas2;
+                            document.getElementById("newTab").href = ssCanvas2;
 
-            }
-        }
-    })
+                        }
+                    }
+                })
+        })
 })
-    
-
-
