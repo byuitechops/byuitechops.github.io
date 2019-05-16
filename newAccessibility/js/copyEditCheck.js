e2ac42fc78472a0364ec3e3297b2f57ed1ef606b
Smoot <cstroop@byui.edu>
2019-05-16 15:15:41 -0600

    small 2

--- newAccessibility/js/copyEditCheck.js
+++ newAccessibility/js/copyEditCheck.js
@@ -41,7 +41,7 @@ function fillReviewStart() {
 
 function fillTranscribeTable(selectedCourseCode) {
     document.getElementById('transcripts-table').innerHTML = '';
-    db.collection("accessibility").where('status', '==', 'Review Completed').orderBy('priority').limit(25).get()
+    db.collection("accessibility").where('status', '==', 'Review Completed').orderBy('priority').get()
         .then(function (querySnapshot) {
             querySnapshot.forEach(function (doc) {
                 if (selectedCourseCode == doc.data().courseCode) {
