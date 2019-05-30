4c51de8aa38909ccb35d2b051e3e5dccc004681f
Hillman <srh235@byui.edu>
2019-05-30 07:45:35 -0600

    Automatic Homepage Slideshow

--- portal/js/main.js
+++ portal/js/main.js
@@ -5,18 +5,18 @@
 'use strict';
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyA_I75-CU5_GlNP1QSKvvH8nbYVkaAUgNA",
-  authDomain: "techopsportal.firebaseapp.com",
-  databaseURL: "https://techopsportal.firebaseio.com",
-  projectId: "techopsportal",
-  storageBucket: "techopsportal.appspot.com",
-  messagingSenderId: "265124430634"
+    apiKey: "AIzaSyA_I75-CU5_GlNP1QSKvvH8nbYVkaAUgNA",
+    authDomain: "techopsportal.firebaseapp.com",
+    databaseURL: "https://techopsportal.firebaseio.com",
+    projectId: "techopsportal",
+    storageBucket: "techopsportal.appspot.com",
+    messagingSenderId: "265124430634"
 };
 firebase.initializeApp(config);
 // Initialize Cloud Firestore through Firebase
 var db = firebase.firestore();
 db.settings({
-  timestampsInSnapshots: true
+    timestampsInSnapshots: true
 });
 
 var userName;
@@ -31,87 +31,95 @@ var auth = firebase.auth();
 
 
 auth.onAuthStateChanged((firebaseUser) => {
-  //checks if the user is already logged in to the system
-  if (firebaseUser) {
-    userName = firebaseUser.displayName;
-    changeTheme();
-    getData();
-    if (window.location.href.includes("index.html") || window.location.href.includes("signup.html") || window.location.pathname == "/") {
-      window.location.replace("home.html");
+    //checks if the user is already logged in to the system
+    if (firebaseUser) {
+        userName = firebaseUser.displayName;
+        changeTheme();
+        getData();
+        if (window.location.href.includes("index.html") || window.location.href.includes("signup.html") || window.location.pathname == "/") {
+        window.location.replace("home.html");
+        }
+    } else {
+        userName = null;
+        if (!window.location.href.includes("index.html") && !window.location.href.includes("signup.html") && !window.location.href.includes("store.html")) {
+        window.location.replace('index.html');
+        }
     }
-  } else {
-    userName = null;
-    if (!window.location.href.includes("index.html") && !window.location.href.includes("signup.html") && !window.location.href.includes("store.html")) {
-      window.location.replace('index.html');
+    if (window.location.href.includes("home.html")) {
+        loadTimer();
+        startTime();
+        plusSlides(1);
+    } else{
+        loadPage();
     }
-  }
-  if (window.location.href.includes("home.html")) {
-    loadTimer();
-    startTime();
-    slideshow(1);
-  } else{
-    loadPage();
-  }
 });
 
 
 function getData() {
-  db.collection("users").where("name", "==", userName)
-    .onSnapshot((querySnapshot) => {
-      data = querySnapshot.docs[0].data();
-      userId = querySnapshot.docs[0].id;
-      preferance = data.viewMode;
-    })
+    db.collection("users").where("name", "==", userName)
+        .onSnapshot((querySnapshot) => {
+            data = querySnapshot.docs[0].data();
+            userId = querySnapshot.docs[0].id;
+            preferance = data.viewMode;
+        })
 }
 
 function editDate(date) {
-  var month = ("0" + (date.getMonth() + 1)).slice(-2);
-  var day = ("0" + date.getDate()).slice(-2);
-  var year = date.getFullYear();
-  var hour = ("0" + date.getHours()).slice(-2);
-  var minute = ("0" + date.getMinutes()).slice(-2);
-  var setDate = `${year}-${month}-${day} ${hour}:${minute}`;
-  return setDate;
+    var month = ("0" + (date.getMonth() + 1)).slice(-2);
+    var day = ("0" + date.getDate()).slice(-2);
+    var year = date.getFullYear();
+    var hour = ("0" + date.getHours()).slice(-2);
+    var minute = ("0" + date.getMinutes()).slice(-2);
+    var setDate = `${year}-${month}-${day} ${hour}:${minute}`;
+    return setDate;
 }
 
 function searchArray(array, item) {
-  for (var i = 0; i < array.length; i++) {
-      if (array[i].name == item) {
-          return i;
-      }
-  }
-  return false;
+    for (var i = 0; i < array.length; i++) {
+        if (array[i].name == item) {
+            return i;
+        }
+    }
+    return false;
 }
 
-var slidesIndex = 1;
 
-function plusSlides(n) {
-  slideshow(slidesIndex += n);
+/*******************************************************
+ * This section is for the slideshow on the home page
+ *******************************************************/
+var slidesIndex = 1;        // Index of which slide is currently active
+var myTimer;                // Automatic timer for the slideshow
+const slideTimer = 8000;    // Each slide appears for 8 seconds.
+
+function plusSlides(n = 1) {
+    clearTimeout(myTimer);
+    changeSlide(slidesIndex += n);
+    myTimer = setTimeout(plusSlides, slideTimer);
 }
 function currentSlide(n) {
-  slideshow(slidesIndex = n);
+    changeSlide(slidesIndex = n);
 }
 
-function slideshow(n) {
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+function changeSlide(n) {
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
 
-  if (n > slides.length) {
-    slidesIndex = 1;
-  }
-  if (n < 1) {
-    slidesIndex = slides.length;
-  }
+    if (n > slides.length) {
+        slidesIndex = 1;
+    }
+    if (n < 1) {
+        slidesIndex = slides.length;
+    }
 
-  for (var i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+    for (var i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
 
-  for (var i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slidesIndex - 1].style.display = "block";
-  dots[slidesIndex - 1].className += " active";
+    slides[slidesIndex - 1].style.display = "block";
+    dots[slidesIndex - 1].className += " active";
 }
 
 
@@ -125,9 +133,9 @@ function slideshow(n) {
 // This const sets a var for all the buttons to change themes
 const dataThemeButtons = document.querySelectorAll('[data-theme]');
 for (let i = 0; i < dataThemeButtons.length; i++) {
-  dataThemeButtons[i].addEventListener('click', () => {
-    changeTheme(i);
-  })
+    dataThemeButtons[i].addEventListener('click', () => {
+        changeTheme(i);
+    })
 }
 
 // This is the main function for theme swapping
@@ -136,101 +144,102 @@ for (let i = 0; i < dataThemeButtons.length; i++) {
 // button somewhere. There is NO NEED to copy and paste new css files.
 // Please and thank you
 function changeTheme(preferance) {
-  if (preferance == 0) {
-    preferance = "light";
-    changeViewMode(preferance);
-  } else if (preferance == 1) {
-    preferance = "dark";
-    changeViewMode(preferance);
-  } else if (preferance == 2) {
-    preferance = "jedi";
-    changeViewMode(preferance);
-  } else if (preferance == 3) {
-    preferance = "sith";
-    changeViewMode(preferance);
-  } else if (preferance == 4) {
-    preferance = "merica";
-    changeViewMode(preferance);
-  }
-  db.collection("users").where("name", "==", userName)
-    .onSnapshot((querySnapshot) => {
-      preferance = querySnapshot.docs[0].data().viewMode;
-      switch (preferance) {
-        case 'light':
-          setTheme({
-            'first': '#0076c6',
-            'second': '#6c757d',
-            'third': '#343a40',
-            'background': '#ffffff',
-            'backgroundSecondary': '#ffffff',
-            'backgroundGrade': 'none',
-            'shadow': 'var(--gray)',
-            'fontPrime': '#000000'
-          });
-          return;
-        case 'dark':
-          setTheme({
-            'first': '#343a40',
-            'second': '#06439F',
-            'third': '#06439F',
-            'background': '#1c1b1b',
-            'backgroundSecondary': '#777777',
-            'backgroundGrade': 'none',
-            'shadow': 'none',
-            'fontPrime': '#ffffff'
-          });
-          return;
-        case 'jedi':
-          setTheme({
-            'first': '#06439F',
-            'second': '#343a40',
-            'third': '#343a40',
-            'background': 'none',
-            'backgroundSecondary': '#ffffff',
-            'backgroundGrade': 'linear-gradient(180deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)',
-            'shadow': 'var(--gray-dark)',
-            'fontPrime': '#000000'
-          });
-          return;
-        case 'sith':
-          setTheme({
-            'first': '#343a40',
-            'second': '#343a40',
-            'third': '#890000',
-            'background': '#1c1b1b',
-            'backgroundSecondary': '#777777',
-            'backgroundGrade': 'linear-gradient(180deg, rgba(36,0,0,1) 0%, rgba(121,9,9,1) 35%, rgba(255,0,0,1) 100%)',
-            'shadow': 'none',
-            'fontPrime': '#ffffff'
-          });
-          return;
-        case 'merica':
-          setTheme({
-            'first': '#0076c6',
-            'second': '#343a40',
-            'third': '#ed2525',
-            'background': '#ffffff',
-            'backgroundSecondary': '#ffffff',
-            'backgroundGrade': 'linear-gradient(9deg, rgba(255,0,0,1) 0%, rgba(221,59,59,1) 16%, rgba(235,208,208,1) 32%, rgba(255,255,255,1) 39%, rgba(191,230,241,1) 45%, rgba(69,163,190,1) 64%, rgba(51,83,185,1) 80%)',
-            'shadow': 'var(--gray)',
-            'fontPrime': '#000000'
-          });
-          return;
-      }
-    })
+    if (preferance == 0) {
+        preferance = "light";
+        changeViewMode(preferance);
+    } else if (preferance == 1) {
+        preferance = "dark";
+        changeViewMode(preferance);
+    } else if (preferance == 2) {
+        preferance = "jedi";
+        changeViewMode(preferance);
+    } else if (preferance == 3) {
+        preferance = "sith";
+        changeViewMode(preferance);
+    } else if (preferance == 4) {
+        preferance = "merica";
+        changeViewMode(preferance);
+    }
+  
+    db.collection("users").where("name", "==", userName)
+        .onSnapshot((querySnapshot) => {
+            preferance = querySnapshot.docs[0].data().viewMode;
+            switch (preferance) {
+                case 'light':
+                    setTheme({
+                        'first': '#0076c6',
+                        'second': '#6c757d',
+                        'third': '#343a40',
+                        'background': '#ffffff',
+                        'backgroundSecondary': '#ffffff',
+                        'backgroundGrade': 'none',
+                        'shadow': 'var(--gray)',
+                        'fontPrime': '#000000'
+                    });
+                    break;
+                case 'dark':
+                    setTheme({
+                        'first': '#343a40',
+                        'second': '#06439F',
+                        'third': '#06439F',
+                        'background': '#1c1b1b',
+                        'backgroundSecondary': '#777777',
+                        'backgroundGrade': 'none',
+                        'shadow': 'none',
+                        'fontPrime': '#ffffff'
+                    });
+                    break;
+                case 'jedi':
+                    setTheme({
+                        'first': '#06439F',
+                        'second': '#343a40',
+                        'third': '#343a40',
+                        'background': 'none',
+                        'backgroundSecondary': '#ffffff',
+                        'backgroundGrade': 'linear-gradient(180deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)',
+                        'shadow': 'var(--gray-dark)',
+                        'fontPrime': '#000000'
+                    });
+                    break;
+                case 'sith':
+                    setTheme({
+                        'first': '#343a40',
+                        'second': '#343a40',
+                        'third': '#890000',
+                        'background': '#1c1b1b',
+                        'backgroundSecondary': '#777777',
+                        'backgroundGrade': 'linear-gradient(180deg, rgba(36,0,0,1) 0%, rgba(121,9,9,1) 35%, rgba(255,0,0,1) 100%)',
+                        'shadow': 'none',
+                        'fontPrime': '#ffffff'
+                    });
+                    break;
+                case 'merica':
+                    setTheme({
+                        'first': '#0076c6',
+                        'second': '#343a40',
+                        'third': '#ed2525',
+                        'background': '#ffffff',
+                        'backgroundSecondary': '#ffffff',
+                        'backgroundGrade': 'linear-gradient(9deg, rgba(255,0,0,1) 0%, rgba(221,59,59,1) 16%, rgba(235,208,208,1) 32%, rgba(255,255,255,1) 39%, rgba(191,230,241,1) 45%, rgba(69,163,190,1) 64%, rgba(51,83,185,1) 80%)',
+                        'shadow': 'var(--gray)',
+                        'fontPrime': '#000000'
+                    });
+                    break;
+            }
+        })
 
 }
 
 // This function draws from and saves to the database.
 function changeViewMode(newTheme) {
-  db.collection('users').doc(userId).update({
-      viewMode: newTheme
-    })
-    .then(function () {})
-    .catch(function (error) {
-      // The document probably doesn't exist.
-      console.error("Error updating document: ", error);
-    });
+    db.collection('users').doc(userId).update({
+        viewMode: newTheme
+        })
+        .then(function () {})
+        .catch(function (error) {
+        // The document probably doesn't exist.
+        console.error("Error updating document: ", error);
+        });
 
 }
 
@@ -241,23 +250,23 @@ function changeViewMode(newTheme) {
 // Ashley and the internet. AN AVACADO, THANKS!
 
 function setTheme(options) {
-  for (let option of Object.keys(options)) {
-    const property = option;
-    const value = options[option];
+    for (let option of Object.keys(options)) {
+        const property = option;
+        const value = options[option];
 
-    setValue(property, value);
-    localStorage.setItem(property, value);
-  }
+        setValue(property, value);
+        localStorage.setItem(property, value);
+    }
 }
 
 function setValue(property, value) {
-  if (value) {
-    document.documentElement.style.setProperty(`--${property}`, value);
+    if (value) {
+        document.documentElement.style.setProperty(`--${property}`, value);
 
-    const input = document.querySelector(`#${property}`);
-    if (input) {
-      value = value.replace('px', '');
-      input.value = value;
+        const input = document.querySelector(`#${property}`);
+        if (input) {
+        value = value.replace('px', '');
+        input.value = value;
+        }
     }
-  }
 }
