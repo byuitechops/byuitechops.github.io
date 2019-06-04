e9e066118aee51f9ead5ac8a922b402e6c94ab4d
Hillman <srh235@byui.edu>
2019-06-04 17:38:45 -0600

    asd

--- portal/js/index.js
+++ portal/js/index.js
@@ -1,45 +1,50 @@
 const signupBtn = document.getElementById("btnsignup");
 const loginBtn = document.getElementById("btnlogin");
+const nameBox = document.getElementById("namebox");
 function loadPage(){};
 
 function login() {
-  var login = document.getElementById('login').value;
-  var password = document.getElementById('password').value;
-  firebase.auth().signInWithEmailAndPassword(login, password)
-    .then(function () {
-      window.location.replace('home.html');
-    })
-    .catch(function (error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      alert('Please, check information provided and try again.');
-      // ...
-    });
+    var login = document.getElementById('login').value;
+    var password = document.getElementById('password').value;
+    firebase.auth().signInWithEmailAndPassword(login, password)
+        .then(function () {
+            window.location.replace('home.html');
+        })
+        .catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            alert('Please, check information provided and try again.');
+            // ...
+        });
 };
+
 $(loginBtn).click(() =>{
-  login();
-});
-$(signupBtn).click(() => {
-  window.location.replace('signup.html');
-});
-window.addEventListener('keyup', () => {
-  event.preventDefault();
-  if (event.keyCode === 13) {
     login();
-  }
 });
+
+function signup() {
+    nameBox.classList.remove("hide");
+}
+
+window.addEventListener('keyup', () => {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        login();
+    }
+});
+
 $("#recover").click(() => {
-  var email = prompt("Please enter your email");
-  if (email == null || email == "") {
-    alert("Please, type a valid email first");
-    return;
-  } else {
-    auth.sendPasswordResetEmail(email).then(function () {
-      alert("An email has been sent. Check your email to change your password");
-    }).catch(function (error) {
-      // An error happened.
-      alert("An error happened, please try again");
-    });
-  }
+    var email = prompt("Please enter your email");
+    if (email == null || email == "") {
+        alert("Please, type a valid email first");
+        return;
+    } else {
+        auth.sendPasswordResetEmail(email).then(function () {
+        alert("An email has been sent. Check your email to change your password");
+        }).catch(function (error) {
+        // An error happened.
+        alert("An error happened, please try again");
+        });
+    }
 });
