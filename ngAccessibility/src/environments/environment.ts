57614af5113563079784ca0479f06b5b4c648f20
XeoSpheric <cstroop@byui.edu>
2019-06-15 09:47:43 -0600

    added search

--- ngAccessibility/src/environments/environment.ts
+++ ngAccessibility/src/environments/environment.ts
@@ -12,6 +12,10 @@ export const environment = {
     storageBucket: 'byui-accessibility-redemption.appspot.com',
     messagingSenderId: '630332651011',
     appId: '1:630332651011:web:c8806a3115d91b70'
+  },
+  algolia: {
+    appId: '6VK394JGGI',
+    apiKey: 'dcec0491e3d9c5748090a98d77a31cc6',
   }
 };
 
