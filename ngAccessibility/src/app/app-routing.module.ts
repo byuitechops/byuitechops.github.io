47c7563d66868befb7d9dfcb2e0e22749a0850e2
Hillman <srh235@byui.edu>
2019-06-18 09:23:58 -0600

    asd

--- ngAccessibility/src/app/app-routing.module.ts
+++ ngAccessibility/src/app/app-routing.module.ts
@@ -29,6 +29,10 @@ const routes: Routes = [
     {
         path: 'transcription/:step',
         component: SearchComponent
+    },
+    {
+        path: '**',
+        component: HomeComponent
     }
 ];
 
