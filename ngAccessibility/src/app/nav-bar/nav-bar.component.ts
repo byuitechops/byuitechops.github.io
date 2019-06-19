7441be2eb0e8de759136e173798176ae3bb4ff9e
XeoSpheric <cstroop@byui.edu>
2019-06-19 09:11:22 -0600

    nav-bar algolia search w/o functions to moderate

--- ngAccessibility/src/app/nav-bar/nav-bar.component.ts
+++ ngAccessibility/src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchService } from '../core/search.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public search: SearchService) { }
 
   ngOnInit() {
   }
