add17032047fc97c54f3cd49bf200f9b886b22ae
XeoSpheric <cstroop@byui.edu>
2019-06-19 09:21:57 -0600

    algolia nav-bar functions

--- ngAccessibility/src/app/nav-bar/nav-bar.component.ts
+++ ngAccessibility/src/app/nav-bar/nav-bar.component.ts
@@ -8,9 +8,18 @@ import { SearchService } from '../core/search.service';
 })
 export class NavBarComponent implements OnInit {
 
+  showResults = false;
+
   constructor(public search: SearchService) { }
 
   ngOnInit() {
   }
 
+  searchChanged(query) {
+    if (query.length >= 3) {
+      this.showResults = true;
+    } else {
+      this.showResults = false;
+    }
+  }
 }
