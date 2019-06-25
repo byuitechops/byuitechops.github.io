c5acf08c1f0ed3ab6e45a6e6a1885bb01755babe
Hillman <srh235@byui.edu>
2019-06-25 17:59:23 -0600

    a;sldfjk

--- ngAccessibility/src/app/request/request.component.ts
+++ ngAccessibility/src/app/request/request.component.ts
@@ -5,45 +5,45 @@ import { element } from '@angular/core/src/render3/instructions';
 import { SearchService } from '../core/search.service';
 
 @Component({
-  selector: 'app-request',
-  templateUrl: './request.component.html',
-  styleUrls: ['./request.component.css']
+    selector: 'app-request',
+    templateUrl: './request.component.html',
+    styleUrls: ['./request.component.css']
 })
 export class RequestComponent implements OnInit {
-  selectUndefinedOptionValue: any;
-  name: string;
-  lms: string;
-  media: string;
-  title: string;
-  priority: string;
-  priorities = [
-    '1 ADA Emergency of Special Request',
-    '2 Improvement Project for Live Course',
-    '3 New/Re-Development Project',
-    '4 Transcript Project'
-  ];
-  type: string;
-  types = [
-    'Video',
-    'Audio',
-    'Alt Text',
-    'Slide'
-  ];
-  course: string;
-  comments: string;
-  override = false;
+    selectUndefinedOptionValue: any;
+    name: string;
+    lms: string;
+    media: string;
+    title: string;
+    priority: string;
+    priorities = [
+        '1 ADA Emergency of Special Request',
+        '2 Improvement Project for Live Course',
+        '3 New/Re-Development Project',
+        '4 Transcript Project'
+    ];
+    type: string;
+    types = [
+        'Video',
+        'Audio',
+        'Alt Text',
+        'Slide'
+    ];
+    course: string;
+    comments: string;
+    override = false;
 
-  dups;
-  constructor(private db: DatabaseService, public auth: AuthService, private search: SearchService) {
+    dups;
+    constructor(private db: DatabaseService, public auth: AuthService, private search: SearchService) {
 
-   }
+}
 
-  ngOnInit() {
+ngOnInit() {
     this.getCourse();
     this.db.checkAction();
-  }
+}
 
-  getCourse() {
+getCourse() {
     const courses = [];
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -56,9 +56,9 @@ export class RequestComponent implements OnInit {
                     const newres = JSON.parse(this.responseText);
                     let inject = '';
                     newres.forEach((doc) => {
-                      const course = doc.__catalogCourseId;
-                      inject += `<option>${course}</option>`;
-                      courses.push(course);
+                        const course = doc.__catalogCourseId;
+                        inject += `<option>${course}</option>`;
+                        courses.push(course);
                     });
                     document.getElementById('requestCourse').insertAdjacentHTML('afterend', inject);
                     return courses;
@@ -71,93 +71,128 @@ export class RequestComponent implements OnInit {
     xhttp.open('GET', 'https://byui.kuali.co/api/v1/catalog/public/catalogs/current', true);
     xhttp.send();
     const html = `<option value="ENG106">ENG106</option>
-                        <option value="ENG106L">ENG106L</option>
-                        <option value="GSPC120L">GSPC120L</option>
-                        <option value="MATH100G">MATH100G</option>
-                        <option value="MATH100L">MATH100L</option>
-                        <option value="PC101">PC101</option>
-                        <option value="PC101L">PC101L</option>
-                        <option value="PC102">PC102</option>
-                        <option value="PC102L">PC102L</option>
-                        <option value="PC103">PC103</option>
-                        <option value="RELPC121">RELPC121</option>
-                        <option value="RELPC122">RELPC122</option>
-                        <option value="FDREL250">FDREL250</option>`;
+                  <option value="ENG106L">ENG106L</option>
+                  <option value="GSPC120L">GSPC120L</option>
+                  <option value="MATH100G">MATH100G</option>
+                  <option value="MATH100L">MATH100L</option>
+                  <option value="PC101">PC101</option>
+                  <option value="PC101L">PC101L</option>
+                  <option value="PC102">PC102</option>
+                  <option value="PC102L">PC102L</option>
+                  <option value="PC103">PC103</option>
+                  <option value="RELPC121">RELPC121</option>
+                  <option value="RELPC122">RELPC122</option>
+                  <option value="FDREL250">FDREL250</option>`;
     document.getElementById('requestCourse').insertAdjacentHTML('beforeend', html);
-  }
+}
 
 
 
 
-  async newRequest() {
+async newRequest() {
+    
     if (this.course === undefined && this.type === undefined) {
-      if ((this.lms === '' || this.lms === undefined) && (this.media === '' || this.media === undefined)) {
-        if ((this.title === '' || this.title === undefined) && this.priority === undefined) {
+        if ((this.lms === '' || this.lms === undefined) && (this.media === '' || this.media === undefined)) {
+            if ((this.title === '' || this.title === undefined) && this.priority === undefined) {
 
-          console.log('uh oh 1');
+                console.log('uh oh 1');
+            }
+            console.log('uh oh 2');
+            alert('Please fill in all fields');
+        } else {
+            let displayName: string;
+
+            if (this.auth.user.isAnonymous) {
+                displayName = this.name;
+            } else {
+                displayName = this.db.user.name;
+            }
+
+            if (this.comments === undefined || this.comments === '') {
+                this.comments = '';
+            } else {
+                this.comments = this.comments + 'Made by ' + displayName;
+            }
+            const data = {
+                backupCode: this.course,
+                copied: false,
+                courseCode: [this.course],
+                datePrepareFinished: '',
+                docEditURL: '',
+                docPublishURL: '',
+                length: '',
+                lmsURL: this.lms,
+                parentTranscript: true,
+                preparer: '',
+                priority: this.priority,
+                requestDate: new Date(),
+                requestNotes: this.comments,
+                requestor: displayName,
+                srcURL: this.media,
+                status: 'Ready for Prep',
+                title: this.title,
+                type: this.type,
+                verbit: false,
+                verbitID: ''
+            };
+
+            console.log(data);
+            this.dups = await this.search.dupCheck(this.title, this.type, this.media);
+
+            if (this.override) {
+                this.db.createTranscript(data);
+                this.override = false;
+            } else if (this.dups.length > 0) {
+                console.log(this.dups);
+            } else {
+                this.db.createTranscript(data);
+            }
+
+            this.comments = '';
+            this.media = '';
+            this.type = undefined;
         }
-        console.log('uh oh 2');
-        alert('Please fill in all fields');
-      }
-      console.log('uh oh 3');
-      alert('Please fill in all fields');
-    } else {
-      let displayName: string;
-      if (this.auth.user.isAnonymous) {
-        displayName = this.name;
-      } else {
-        displayName = this.db.user.name;
-      }
-      if (this.comments === undefined || this.comments === '') {
-        this.comments = '';
-      } else {
-        this.comments = this.comments + 'Made by ' + displayName;
-      }
-      const data = {
-        backupCode: this.course,
-        copied: false,
-        courseCode: [this.course],
-        datePrepareFinished: '',
-        docEditURL: '',
-        docPublishURL: '',
-        length: '',
-        lmsURL: this.lms,
-        parentTranscript: true,
-        preparer: '',
-        priority: this.priority,
-        requestDate: new Date(),
-        requestNotes: this.comments,
-        requestor: displayName,
-        srcURL: this.media,
-        status: 'Ready for Prep',
-        title: this.title,
-        type: this.type,
-        verbit: false,
-        verbitID: ''
-      };
-      console.log(data);
-      this.dups = await this.search.dupCheck(this.title, this.type, this.media);
-      if (this.override) {
-        this.db.createTranscript(data);
-        this.override = false;
-      } else if (this.dups.length > 0) {
-        console.log(this.dups);
-      } else {
-        this.db.createTranscript(data);
-      }
-      this.comments = '';
-      this.media = '';
-      this.type = undefined;
     }
-  }
+}
 
-  useDuplicate(id) {
+useDuplicate(id) {
     this.db.addCourseCode(id, this.course);
-  }
+}
 
-  createNew() {
+createNew() {
     this.override = true;
     this.newRequest();
-  }
+}
+
+openDup() {
+    const dup     = document.getElementById("dup-modal");
+    const navbar  = document.getElementById("main-nav");
+    const content = document.getElementsByTagName("main");
+
+    dup.classList.remove("hide");
+    navbar.classList.add("blur");
+    for(let i = 0; i < content.length; i++) {
+        content[i].classList.add("blur");
+    }
+}
+
+closeDup() {
+    const dup     = document.getElementById("dup-modal");
+    const navbar  = document.getElementById("main-nav");
+    const content = document.getElementsByTagName("main");
+
+    dup.classList.add("hide");
+    navbar.classList.remove("blur");
+    for(let i = 0; i < content.length; i++) {
+        content[i].classList.remove("blur");
+    }
+}
+
+clickedOut(event) {
+    if (event.target.className === "modal") {
+        this.closeDup();
+    }
+}
+
 }
 
