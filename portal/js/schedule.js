de20ce2b467aaff3916d7677eb4afdef3b30b644
Hillman <srh235@byui.edu>
2019-07-29 07:16:24 -0600

    Hopefully this updates the schedules

--- portal/js/schedule.js
+++ portal/js/schedule.js
@@ -1,8 +1,8 @@
-const ssCanvas2 = "https://docs.google.com/spreadsheets/d/1jUFL2Dr-_MjAE7axb6rEZy0vSfH6y_8fOHSo1b0oLDo/edit?rm=minimal";
-const ssCanvas1 = "https://docs.google.com/spreadsheets/d/1g53WojmX1wG7fTGU1Q1Hg93-QJ5GBadcCU7rbeyGWoc/edit?rm=minimal";
+const ssCanvas2 = "https://docs.google.com/spreadsheets/d/1DdQfPe-JuB0pjPQ1qTUmFHl6RlGG3WJPyS_tcogQ-Zs/edit#gid=0";
+const ssCanvas1 = "https://docs.google.com/spreadsheets/d/1I8_4D8T4mXJOlR9GysuuCDvHq2qlpcjCUNfjorphqKs/edit#gid=0";
 const team = document.getElementById("teams");
 const iframe = document.getElementById("spreadsheet");
-const teamSelector = document.getElementById("selectTeam")
+const teamSelector = document.getElementById("selectTeam");
 const newTab = document.getElementById("newTab");
 
 function loadPage() {
