5de6eaccc53a21f975a62c87134bea09999baa75
Hillman <srh235@byui.edu>
2019-08-05 15:21:09 -0600

    Working Clock and Break Button

--- ngPortal/src/app/home/home.component.ts
+++ ngPortal/src/app/home/home.component.ts
@@ -3,10 +3,6 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { AuthService } from '../core/auth.service';
 import { splitDepsDsl } from '@angular/core/src/view/util';
 
-@Pipe({
-    name: 'today',
-    pure: false
-})
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
@@ -24,6 +20,7 @@ export class HomeComponent implements OnInit {
     };
     breakBtn = 'Start Break';
     checkMsg = '';
+    breakMsg = '';
     today;
 
     constructor(public db: AngularFirestore, private auth: AuthService) { }
@@ -32,23 +29,28 @@ export class HomeComponent implements OnInit {
         this.checkTime();
         this.hideOthers();
         this.clock();
+        //this.autoSlide();
     }
 
     clock() {
-        this.today = Date.now();
-        setTimeout(this.clock, 1000);
+        setInterval(() => {
+            this.today = Date.now();
+        }, 1000);
     }
 
     checkTime() {
         setTimeout(() => {
             try {
                 this.db.collection('users').doc(this.auth.id).get().subscribe(doc => {
-                this.time = doc.data().time;
-                if (this.time.check) {
-                    this.checkMsg = 'Clock in time was ' + (doc.data().time.checkKey).slice(-5);
-                } else if (!this.time.check) {
-                    this.checkMsg = 'Clock out time was ' + (doc.data().time.checkKey).slice(-5);
-                }
+                    this.time = doc.data().time;
+                    if (this.time.check) {
+                        this.checkMsg = 'Clock in time was ' + (doc.data().time.checkKey).slice(-5);
+                    } else if (!this.time.check) {
+                        this.checkMsg = 'Clock out time was ' + (doc.data().time.checkKey).slice(-5);
+                    }
+                    if (!this.time.break) {
+                        this.breakMsg = 'Last break finished at ' + (doc.data().time.breakKey).slice(-5);
+                    }
                 });
             } catch (err) {
                 setTimeout(() => {
@@ -62,26 +64,26 @@ export class HomeComponent implements OnInit {
     clockInOut() {
         const setDate = this.editDate(new Date());
         if (!this.time.check) {
-        this.db.collection('users').doc(this.auth.id).update({
-            'time.checkKey': setDate,
-            'time.check': true,
-            'time.breakAllowed': false
-        });
-        this.db.collection('users').doc(this.auth.id).collection('hoursWorked').doc(setDate).set({
-            start: setDate.slice(-5)
-        });
-        window.open('https://www.myworkday.com/byuhi/d/home.htmld', '_blank');
-        window.open('https://chat.google.com/room/AAAAGWGIzV4', '_blank');
+            this.db.collection('users').doc(this.auth.id).update({
+                'time.checkKey': setDate,
+                'time.check': true,
+                'time.breakAllowed': false
+            });
+            this.db.collection('users').doc(this.auth.id).collection('hoursWorked').doc(setDate).set({
+                start: setDate.slice(-5)
+            });
+            window.open('https://www.myworkday.com/byuhi/d/home.htmld', '_blank');
+            window.open('https://chat.google.com/room/AAAAGWGIzV4', '_blank');
         } else if (this.time.check) {
-        this.db.collection('users').doc(this.auth.id).update({
-            'time.checkKey': setDate,
-            'time.check': false,
-            'time.breakAllowed': false
-        });
-        this.db.collection('users').doc(this.auth.id).collection('hoursWorked').doc(setDate).set({
-            end: setDate.slice(-5)
-        });
-        window.open('https://www.myworkday.com/byuhi/d/home.htmld', '_blank');
+            this.db.collection('users').doc(this.auth.id).update({
+                'time.checkKey': setDate,
+                'time.check': false,
+                'time.breakAllowed': false
+            });
+            this.db.collection('users').doc(this.auth.id).collection('hoursWorked').doc(setDate).set({
+                end: setDate.slice(-5)
+            });
+            window.open('https://www.myworkday.com/byuhi/d/home.htmld', '_blank');
         }
         setTimeout(() => {
             this.checkTime();
@@ -118,13 +120,58 @@ export class HomeComponent implements OnInit {
     prevSlide() {
         this.nowThisSlide(((this.current + this.size) - 1) % this.size);
     }
-
-    takeBreak() {
-        document.getElementById('break-btn').classList.toggle('expanded');
-        document.getElementById('break-words').classList.toggle('hide');
+    autoSlide() {
+        setInterval(() => {
+            this.nowThisSlide((this.current + 1) % this.size);
+        }, 10000);
     }
 
-    breakMins: number;
-    breakSecs: number;
-    
+    breakMins: number = 0;
+    breakSecs: number = 5;
+    zero: string = '0';
+    breakTimer: any;
+
+    takeBreak() {
+        const setDate = this.editDate(new Date());
+        document.getElementById('break-words').classList.toggle('hide');
+        document.getElementById('break-btn').classList.toggle('expanded');
+        
+        if(!this.time.break) {
+            this.startBreak();
+        } else if (this.time.break) {
+            this.endBreak();
+        }
+        this.time.break = !this.time.break;
+        this.time.breakKey = setDate;
+    }
+    startBreak() {
+        this.breakTimer = setInterval(() => {
+            if (this.breakMins == 0 && this.breakSecs == 0) {
+                this.breakMins = 15;
+            } else if (this.breakMins > 0 && this.breakSecs == 0) {
+                this.breakMins--;
+                this.breakSecs = 59;
+            } else {
+                this.breakSecs--;
+            }
+            if (this.breakSecs < 10) {
+                this.zero = '0';
+            } else {
+                this.zero = '';
+            }
+            this.checkBreak();
+        }, 1000);
+    }
+    endBreak() {
+        clearInterval(this.breakTimer);
+        document.getElementById('break-btn').classList.remove('warning', 'danger');
+    }
+    checkBreak() {
+        if (this.breakMins == 0 && this.breakSecs == 0) {
+            document.getElementById('break-btn').classList.remove('warning');
+            document.getElementById('break-btn').classList.add('danger');
+        } else if (this.breakMins == 0 && this.breakSecs <= 59) {
+            document.getElementById('break-btn').classList.add('warning');
+        }
+    }
 }
