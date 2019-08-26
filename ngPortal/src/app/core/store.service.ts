7a0214df42901a34f4ab100a6ea9f0da032f541b
Hillman <srh235@byui.edu>
2019-08-26 10:18:07 -0600

    asfl;k

--- ngPortal/src/app/core/store.service.ts
+++ ngPortal/src/app/core/store.service.ts
@@ -3,39 +3,77 @@ import * as firebase from 'firebase/app/';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireStorage } from '@angular/fire/storage';
 
+import { AuthService } from '../core/auth.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class StoreService {
 
-  items = [];
-  edit = false;
-  total = 0.00;
-  constructor(private db: AngularFirestore, private storage: AngularFireStorage) {
-    const stuff = db.collection('store').doc('inventory').collection('items').get();
-    stuff.forEach(raw => {
-      raw.forEach(async item => {
-        const data = await item.data();
-        const id = await item.id;
-        const ref = storage.ref(`images/${data.image}`);
-        const url = ref.getDownloadURL();
-        this.items.push({
-          ...data,
-          url,
-          id
-        });
-      });
-    });
-  }
+    user: any;
+    edit: boolean = false;
+    total: number = 0;
+    items = [];
+    cart = [];
+    count = this.cart.length;
 
-  addToCart(id, price, count) {
-    if (count > 0) {
-      this.total += Number(price);
-      this.items.forEach(item => {
-        if (item.id === id) {
-          item.count = count - 1;
-        }
-      });
+    constructor(
+        private db: AngularFirestore, 
+        private storage: AngularFireStorage, 
+        public auth: AuthService,
+    ) {
+        const stuff = db.collection('store').doc('inventory').collection('items').get();
+        stuff.forEach(raw => {
+            raw.forEach(async item => {
+                const data = await item.data();
+                const id = await item.id;
+                const ref = storage.ref(`images/${data.image}`);
+                const url = ref.getDownloadURL();
+                this.items.push({
+                    ...data,
+                    url,
+                    id
+                });
+            });
+        });
     }
-  }
+
+    addToCart(item) {
+        this.cart.push(item);
+        this.sumTotal();
+        this.lowerItemCount(item);
+        console.log(this.cart.length);
+    }
+
+    sumTotal() {
+        this.total = 0;
+        this.cart.forEach(item => {
+            this.total += item.price;
+        });
+    }
+    
+    lowerItemCount(item) {
+        item.count--;
+    }
+
+    raiseItemCount(item) {
+        item.count++;
+    }
+
+    removeItem(item) {
+        console.log(this.cart.indexOf(item));
+        this.cart.splice(this.cart.indexOf(item), 1);
+        this.raiseItemCount(item);
+        this.sumTotal();
+    }
+
+    clearCart() {
+        while(this.cart.length > 0) {
+            this.removeItem(this.cart[0]);
+        }
+    }
+
+    checkout() {
+        
+    }
 }
--- ngPortal/src/app/store/store.component.ts
+++ ngPortal/src/app/store/store.component.ts
@@ -3,24 +3,15 @@ import { StoreService } from '../core/store.service';
 import { AuthService } from '../core/auth.service';
 
 @Component({
-  selector: 'app-store',
-  templateUrl: './store.component.html',
-  styleUrls: ['./store.component.css']
+    selector: 'app-store',
+    templateUrl: './store.component.html',
+    styleUrls: ['./store.component.css']
 })
 export class StoreComponent implements OnInit {
 
     constructor(public store: StoreService, public auth: AuthService) { }
 
     ngOnInit() {
-        this.canUserManageStore();
-    }
-
-
-    userManage: boolean = false;
-    canUserManageStore() {
-        if (this.auth.user.admin || this.auth.user.storeManager || this.auth.user.lead) {
-            this.userManage = true;
-        }
     }
 
 }
